d17342a92ef85151ca7094e084381f48a4a299b9
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-06-14 15:12:31 +0800

    Model for base layer. ref #34

--- backend/суурь_давхарга/models.py
+++ backend/суурь_давхарга/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+
+class СуурьДавхарга(models.Model):
+
+    name = models.CharField(max_length=250)
+    url = models.CharField(max_length=500)
+    thumbnail_1x = models.ImageField(upload_to='суурь_давхарга/thumbnail_1x/')
+    thumbnail_2x = models.ImageField(upload_to='суурь_давхарга/thumbnail_2x/')
