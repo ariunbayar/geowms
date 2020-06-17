a4e50b78a01484a5ce38eb99ec2d339687b1f59d
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-06-17 09:29:52 +0800

    Production webpack. ref #41

--- geoportal/settings/base.py
+++ geoportal/settings/base.py
@@ -69,6 +69,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'geoportal.context_processors.context_processor',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
