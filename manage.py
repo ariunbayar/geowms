dfecdbc9b578ee3b19428e08df3e460c202616fc
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-06-20 16:34:06 +0800

    Fixes devserver. ref #59

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geoportal.settings.mydev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geoportal.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
