0d457fa0fc9e074404cb4e7d8d0e2d3c3aefcf52
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-07-05 21:49:43 +0800

    GovOrg WMSLayer. ref #128

--- backend/govorg/models.py
+++ backend/govorg/models.py
@@ -10,5 +10,17 @@ class GovOrg(models.Model):
     name = models.CharField(max_length=250)
     token = models.CharField(max_length=250)
 
+    wms_layers = models.ManyToManyField(
+        'backend_wmslayer.WMSLayer',
+        through='backend_govorg.GovOrgWMSLayer',
+        related_name='govorgs'
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class GovOrgWMSLayer(models.Model):
+    govorg = models.ForeignKey('backend_govorg.GovOrg', on_delete=models.CASCADE)
+    wms_layer = models.ForeignKey('backend_wmslayer.WMSLayer', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
