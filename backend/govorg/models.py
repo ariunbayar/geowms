fc18413991eff4c586772216cd2ef4ca05780ff1
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-07-05 21:18:33 +0800

    Govorg model. ref #128

--- backend/govorg/models.py
+++ backend/govorg/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class GovOrg(models.Model):
+
+    name = models.CharField(max_length=250)
+    token = models.CharField(max_length=250)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
