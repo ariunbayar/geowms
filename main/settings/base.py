88658bc0dee3276854752cd22a0ec77764178997
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-07-05 21:58:17 +0800

    Fixes login redirection.

--- main/settings/base.py
+++ main/settings/base.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-LOGIN_URL = 'login'
+LOGIN_URL = 'secure:login'
 LOGIN_REDIRECT_URL = 'bundle:all'
 LOGOUT_REDIRECT_URL = 'bundle:all'
 
