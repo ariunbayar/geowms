b766bcab6fd401bc704584c9e62e5637a71005e7
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-07-14 08:39:07 +0800

    Renames config table. ref #95

--- backend/config/models.py
+++ backend/config/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
 class Config(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Агентлагийн мэдээлэл"
+        db_table = "config"
+
     name = models.CharField(max_length=250, db_index=True, verbose_name='Гарчиг')
     value = models.TextField(verbose_name='Товч агуулга')
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        verbose_name_plural = ("Агентлагийн мэдээлэл")
