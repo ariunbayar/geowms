8d60b307e2b373128842261f9f3987def2d63408
delgermaa47 <67996697+Delgermaa47@users.noreply.github.com>
2020-07-21 15:51:11 +0800

    153_newtersen hereglegchiin ner haruulah

--- geoportal_app/models.py
+++ geoportal_app/models.py
@@ -5,34 +5,36 @@ from django.contrib.auth.models import User
 
 
 class Role(models.Model):
-  ROLE1 = 1
-  ROLE2 = 2
-  ROLE3 = 3
-  ROLE4 = 4
-  ADMIN = 5
-  ROLE_CHOICES = (
-      (ROLE1, 'role1'),
-      (ROLE2, 'role2'),
-      (ROLE3, 'role3'),
-      (ROLE4, 'role4'),
-      (ADMIN, 'admin'),
-  )
+    ROLE1 = 1
+    ROLE2 = 2
+    ROLE3 = 3
+    ROLE4 = 4
+    ADMIN = 5
+    ROLE_CHOICES = (
+        (ROLE1, 'role1'),
+        (ROLE2, 'role2'),
+        (ROLE3, 'role3'),
+        (ROLE4, 'role4'),
+        (ADMIN, 'admin'),
+    )
 
-  id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
+    id = models.PositiveSmallIntegerField(
+        choices=ROLE_CHOICES, primary_key=True)
 
-  def __str__(self):
-      return self.get_id_display()
+    def __str__(self):
+        return self.get_id_display()
 
-  class Meta:
-      verbose_name = ("Хэрэглэгчийн эрх")
-      verbose_name_plural = ("Хэрэглэгчийн эрхүүд")
+    class Meta:
+        verbose_name = ("Хэрэглэгчийн эрх")
+        verbose_name_plural = ("Хэрэглэгчийн эрхүүд")
 
 
 class User(AbstractUser):
-  roles = models.ManyToManyField(Role)
-  register = models.CharField(max_length=10, null=True)
-  gender = models.CharField(max_length=10, null=True)
-  is_sso = models.BooleanField(default=False)
+    roles = models.ManyToManyField(Role)
+    register = models.CharField(max_length=10, null=True)
+    gender = models.CharField(max_length=10, null=True)
+    is_sso = models.BooleanField(default=False)
+
 
 class Page(models.Model):
     title = models.CharField(max_length=1000, verbose_name='Гарчиг')
