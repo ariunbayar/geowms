742f0eb007c57baafc646ee2b75df4560347e3fb
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-08-01 18:24:54 +0800

    Sample add employee. ref #255

--- backend/org/views.py
+++ backend/org/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_POST
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import user_passes_test
 
@@ -10,7 +10,7 @@ from .models import Org
 @require_GET
 @ajax_required
 @user_passes_test(lambda u: u.is_superuser)
-def all(request, level, payload=dict()):
+def all(request, level):
 
     level_display = dict((
         (1, '1-р түвшин'),
@@ -36,7 +36,7 @@ def all(request, level, payload=dict()):
 @require_GET
 @ajax_required
 @user_passes_test(lambda u: u.is_superuser)
-def employees(request, level, pk, payload=dict()):
+def employees(request, level, pk):
 
     # org = get_object_or_404(Org, pk=pk)
 
@@ -58,3 +58,33 @@ def employees(request, level, pk, payload=dict()):
         })
 
     return JsonResponse({'employees': employees_display})
+
+
+@require_POST
+@ajax_required
+@user_passes_test(lambda u: u.is_superuser)
+def employee_add(request, payload, level, pk):
+
+    user_id = payload.get('user_id')
+    position = payload.get('position')
+
+    # user = get_object_or_404(User, pk=user_id)
+
+    employees_display = []
+
+    from random import randint
+
+    for i in range(100, randint(105, 120)):
+        employees_display.append({
+            'id': i,
+            'last_name': 'Telmuun%s' % i,
+            'first_name': 'Telmuun%s' % i,
+            'email': 'user_%s@example.com' % i,
+            'is_active': [True, False][randint(0, 1)],
+            'is_sso': [True, False][randint(0, 1)],
+            'position': 'Газар зохион байгуулалт, төлөвлөлт, газар өмчлөлийн асуудал хариуцсан ахлах мэргэжилтэн %s' % i,
+            'created_at': '2020-01-01',
+            'updated_at': '2020-04-17',
+        })
+
+    return JsonResponse({'success': True})
--- backend/urls.py
+++ backend/urls.py
@@ -59,6 +59,7 @@ urlpatterns = [
     path('api/org/', include(([
         path('level-<int:level>/', org_views.all, name='all'),
         path('level-<int:level>/<int:pk>/employees/', org_views.employees, name='employees'),
+        path('level-<int:level>/<int:pk>/employee-add/', org_views.employee_add, name='employee-add'),
     ], 'org'))),
 
     path('api/систем/', include(([
