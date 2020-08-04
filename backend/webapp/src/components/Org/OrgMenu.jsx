06e121a0d5b5c085d87942a77f6b245ea2faadba
Tuguldur88 <43751857+Tuguldur88@users.noreply.github.com>
2020-08-04 11:56:45 +0800

    zasvar

--- backend/webapp/src/components/Org/OrgMenu.jsx
+++ backend/webapp/src/components/Org/OrgMenu.jsx
@@ -43,12 +43,12 @@ export class OrgMenu extends Component {
                                 </NavLink>
                                 <NavLink className="menu" exact to={`/back/байгууллага/түвшин/${org_level}/${org_id}/хэрэглэгч/`} activeClassName="active">
                                     <div className="list-group-item d-flex justify-content-between align-items-center col-md-12">
-                                        хэрэглэгч
+                                        Хэрэглэгч
                                     </div>
                                 </NavLink>
                                 <NavLink className="menu" exact to={`/back/байгууллага/түвшин/${org_level}/${org_id}/систем/`} activeClassName="active">
                                     <div className="list-group-item d-flex justify-content-between align-items-center col-md-12">
-                                        систем
+                                        Систем
                                     </div>
                                 </NavLink>
                             </div>
