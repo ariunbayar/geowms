a4714f5b1c3a8177d24fd37c04f1ac8a085d5b89
Tuguldur88 <43751857+Tuguldur88@users.noreply.github.com>
2020-08-11 10:28:24 +0800

    bank model update

--- backend/payment/models.py
+++ backend/payment/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from geoportal_app.models import User
 # Create your models here.
 class Payment(models.Model):
-    number = models.CharField(max_length=32, unique=True) 
-    amount = models.PositiveIntegerField()
+    geo_unique_number = models.CharField(max_length=300, unique=True)
+    bank_unique_number = models.CharField(max_length=300)
+
+    data_id = models.CharField(max_length=300)
     description = models.CharField(max_length=500)
+    amount = models.PositiveIntegerField()
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
     is_success = models.BooleanField()
-    success_at = models.DateTimeField(auto_now_add=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    success_at = models.DateTimeField(auto_now=True)
+    failed_at = models.DateTimeField(auto_now=True)
+
+    error_message = models.CharField(max_length=500)
+    error_code = models.CharField(max_length=100)
