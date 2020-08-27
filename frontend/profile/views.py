087da8b164b2dae3a1b390eaa3c3e4064f171d97
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-08-27 17:08:36 +0800

    Refactor. ref #421

--- frontend/profile/views.py
+++ frontend/profile/views.py
@@ -1,51 +1,52 @@
-from itertools import groupby
-
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import render, reverse, get_object_or_404
-from django.views.decorators.http import require_GET, require_POST
-import random
+from django.shortcuts import render
+from django.views.decorators.http import require_POST, require_GET
+
 from main.decorators import ajax_required
-from datetime import date
-import datetime
 from backend.payment.models import Payment
-from geoportal_app.models import User
 
 
+@require_GET
+@login_required
 def history(request):
 
     return render(request, 'profile/index.html', {"profile": "profile"})
 
+def _datetime_display(dt):
+    return dt.strftime('%Y-%m-%d') if dt else None
+
+def _get_payment_display(payment):
+
+
+    return {
+        'id': payment.id,
+        'amount': payment.amount,
+        'description': payment.description,
+        'created_at': _datetime_display(payment.created_at),
+        'is_success': payment.is_success,
+        'user_id': payment.user_id,
+        'bank_unique_number': payment.bank_unique_number,
+        'data_id': payment.data_id,
+        'geo_unique_number': payment.geo_unique_number,
+        'success_at': _datetime_display(payment.success_at),
+    }
+
 
 @require_POST
 @ajax_required
-def all(request,payload):
+@login_required
+def all(request, payload):
+
     last = payload.get('last')
     first = payload.get('first')
-    history_all = []
-    for payment in Payment.objects.all()[first:last]:
-        if(payment.created_at):
-            created_at = payment.created_at.strftime('%Y-%m-%d')
-        else:
-            created_at = datetime.datetime(1, 1, 1, 0, 0).strftime('%Y-%m-%d')
 
-        if(payment.success_at):
-            success_at = payment.success_at.strftime('%Y-%m-%d')
-        else:
-            success_at =  datetime.datetime(1, 1, 1, 0, 0).strftime('%Y-%m-%d')
-        history_all.append({
-            'id': payment.id,
-            'amount': payment.amount,
-            'description': payment.description,
-            'created_at': created_at,
-            'is_success': payment.is_success,
-            'user_id':payment.user_id,
-            'bank_unique_number':payment.bank_unique_number,
-            'data_id':payment.data_id,
-            'geo_unique_number':payment.geo_unique_number,
-            'success_at':success_at,
-    })
+    payment_list_display = [
+        _get_payment_display(payment)
+        for payment in Payment.objects.all()[first:last]
+    ]
 
     return JsonResponse({
-        "payment":history_all,
-        'len':Payment.objects.all().count()
+        "payment": payment_list_display,
+        'len': Payment.objects.all().count()
     })
