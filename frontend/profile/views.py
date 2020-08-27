f1584a8d7310d8a2a012a11b0a9fbe2354b80f1e
delgermaa47 <67996697+Delgermaa47@users.noreply.github.com>
2020-08-27 15:49:18 +0800

    421_issue

--- frontend/profile/views.py
+++ frontend/profile/views.py
@@ -23,17 +23,26 @@ def all(request,payload):
     first = payload.get('first')
     history_all = []
     for payment in Payment.objects.all()[first:last]:
+        if(payment.created_at):
+            created_at = payment.created_at.strftime('%Y-%m-%d')
+        else:
+            created_at = datetime.datetime(1, 1, 1, 0, 0).strftime('%Y-%m-%d')
+
+        if(payment.success_at):
+            success_at = payment.success_at.strftime('%Y-%m-%d')
+        else:
+            success_at =  datetime.datetime(1, 1, 1, 0, 0).strftime('%Y-%m-%d')
         history_all.append({
             'id': payment.id,
             'amount': payment.amount,
             'description': payment.description,
-            'created_at': payment.created_at.strftime('%Y-%m-%d'),
+            'created_at': created_at,
             'is_success': payment.is_success,
             'user_id':payment.user_id,
             'bank_unique_number':payment.bank_unique_number,
             'data_id':payment.data_id,
             'geo_unique_number':payment.geo_unique_number,
-            'success_at':payment.success_at.strftime('%Y-%m-%d'),
+            'success_at':success_at,
     })
 
     return JsonResponse({
