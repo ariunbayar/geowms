3a23320e7f934cbf372657b7482e0f99bc108e26
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-08-28 14:38:37 +0800

    Changes static file location

--- main/settings/base.py
+++ main/settings/base.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'geoportal_app', 'static')
 LOGIN_URL = 'secure:login'
 LOGIN_REDIRECT_URL = 'bundle:all'
 LOGIN_REDIRECT_URL_MOBILE = 'mobile:all'
