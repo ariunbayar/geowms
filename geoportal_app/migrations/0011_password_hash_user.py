6712234ddad08e12834fc57461ecc26aaf2bab04
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-08-31 08:15:56 +0800

    Fixes migration. ref #430

--- geoportal_app/migrations/0011_password_hash_user.py
+++ geoportal_app/migrations/0011_password_hash_user.py
@@ -1,17 +1,21 @@
 from django.db import migrations, models
+from django.contrib.auth.hashers import make_password
+
 
 def update_user(apps, schema_editor):
+
     User = apps.get_model('geoportal_app', 'User')
     users = User.objects.all()
-  
+
     for user in users:
         if not user.password:
             continue
         if user.password.startswith('pbkdf2_sha256'):
             continue
         else:
-            user.set_password(user.password)
+            user.password = make_password(user.password)
             user.save()
+
 
 class Migration(migrations.Migration):
 
