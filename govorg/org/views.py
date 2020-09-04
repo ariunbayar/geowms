575bc6708e3b284551e031ed28fea2559430bb72
Baigalmaa Zorigt <49391474+baigalaaZorigt@users.noreply.github.com>
2020-09-04 19:40:16 +0800

    bug

--- govorg/org/views.py
+++ govorg/org/views.py
@@ -8,41 +8,39 @@ from django.http import JsonResponse
 from django.core.paginator import Paginator
 
 
-def _get_employee_display(employe):
+def _get_employee_display(employee):
 
     return {
-        'id': employe.id,
-        'last_name': employe.last_name,
-        'username': employe.username,
-        'first_name': employe.first_name,
-        'email': employe.email,
-        'register': employe.register,
-        'gender': employe.gender,
-        'is_active': employe.is_active,
-        'is_sso': employe.is_sso,
-        'position': Employee.objects.filter(user=employe).values('position')[0]['position'],
-        'created_at': Employee.objects.filter(user=employe).values('created_at')[0]['created_at'].strftime('%Y-%m-%d'),
-        'updated_at': Employee.objects.filter(user=employe).values('updated_at')[0]['updated_at'].strftime('%Y-%m-%d'),
+        'id': employee.id,
+        'last_name': employee.user.last_name,
+        'username': employee.user.username,
+        'first_name': employee.user.first_name,
+        'email': employee.user.email,
+        'register': employee.user.register,
+        'gender': employee.user.gender,
+        'is_active': employee.user.is_active,
+        'is_sso': employee.user.is_sso,
+        'position': employee.position,
+        'created_at': employee.created_at.strftime('%Y-%m-%d'),
+        'updated_at': employee.updated_at.strftime('%Y-%m-%d'),
     }
 
 
 @require_POST
 @ajax_required
 def employees(request, payload):
-
+ 
     page = payload.get('page')
     per_page = payload.get('per_page')
-    User.objects.filter(username="bolormaa")
-    user_id = get_object_or_404(User,username="bolormaa").id
-    org_id = get_object_or_404(Employee, user_id=user_id).org_id
-    level = get_object_or_404(Org, id=org_id).level
-    org = get_object_or_404(Org, pk=org_id, level=level)
-    Emp = User.objects.filter(employee__org=org).order_by('id')
-    total_items = Paginator(Emp, per_page)
+
+    employee = get_object_or_404(Employee, user=request.user)
+    employees = Employee.objects.filter(org=employee.org).order_by('id')
+    total_items = Paginator(employees, per_page)
     items_page = total_items.page(page)
+
     items = [
-        _get_employee_display(pay)
-        for pay in items_page.object_list
+        _get_employee_display(emp)
+        for emp in employees
     ]
     total_page = total_items.num_pages
 
