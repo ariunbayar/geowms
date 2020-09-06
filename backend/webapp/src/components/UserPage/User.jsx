3341e84008fed07a0c2afb8f54b45c18f671f95a
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-09-06 08:04:05 +0800

    Rolls back styling changes and applies the simple edit. ref #503

--- backend/webapp/src/components/UserPage/User.jsx
+++ backend/webapp/src/components/UserPage/User.jsx
@@ -1,47 +1,39 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import React, { Component } from "react"
+import {NavLink} from "react-router-dom"
+
 
 export default class User extends Component {
-    render() {
-        const {
-            id,
-            last_name,
-            first_name,
-            roles,
-            email,
-            is_active,
-            is_sso,
-        } = this.props.values;
 
-        const idx = this.props.idx;
+    render() {
+
+        const {id, last_name, first_name, roles, email, is_active, is_sso} = this.props.values
+
+        const idx=this.props.idx
         return (
             <tr>
-                <td scope="col">{idx}</td>
+                <td scope="col">
+                    {idx}
+                </td>
                 <td scope="col">
                     <NavLink to={`/back/user/${id}/дэлгэрэнгүй/`}>
-                        <strong>
-                            {last_name.charAt(0).toUpperCase()}.{first_name}
-                        </strong>
+                        <strong>{last_name.charAt(0).toUpperCase()}.{first_name}</strong>
                     </NavLink>
                 </td>
-                <td>{email}</td>
-                <td>{roles.map((role) => role.name)}</td>
                 <td>
-                    {is_active ? (
-                        <a className="text-primary">Идэвхтэй </a>
-                    ) : (
-                        <a className="text-muted">Идэвхгүй</a>
-                    )}
+                    {email}
                 </td>
                 <td>
-                    {is_sso && (
-                        <img
-                            className="dan-logo-icon"
-                            src="/static/assets/image/logo/dan-logo2.png"
-                        />
-                    )}
+                    { roles.map(role=>role.name) }
+                </td>
+                <td className={ is_active ? "text-primary" : "text-muted" }>
+                    { is_active ? "Идэвхтэй" : "Идэвхгүй" }
+                </td>
+                <td>
+                    {is_sso &&
+                        <img className="dan-logo-icon" src="/static/assets/image/logo/dan-logo2.png"/>
+                    }
                 </td>
             </tr>
-        );
+        )
     }
 }
