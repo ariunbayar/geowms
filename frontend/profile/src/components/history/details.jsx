9e046663850c0fc9aa67e69127a45e562d44be13
odko77 <63903818+odko77@users.noreply.github.com>
2020-09-13 16:02:17 +0800

    Профайл худалдан авалт дэлгэрэнгүй

---
+++ frontend/profile/src/components/history/details.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from "react"
+import { service } from "./service"
+import { NavLink } from "react-router-dom"
+
+export class Details extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            items: [],
+            check_error: false,
+            error_msg: '',
+            points: [],
+            payment_id: this.props.match.params.id,
+            check_info: false,
+        }
+    }
+
+    componentDidMount(){
+        const id = this.state.payment_id
+        service.getDetails(id).then(({success, items, points}) => {
+            if(success){
+                items.map(( items ) =>
+                    this.setState({items})
+                )
+                this.setState({points})
+            }
+            else{
+                this.setState({ check_info: !success })
+            }
+        }).catch(error => console.log(error))
+    }
+
+    render() {
+        const { items, check_error, error_msg, points, check_info } = this.state
+        console.log(items)
+        return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 py-0 my-3">
+                    <NavLink to="/profile/all/" className="btn mt-2 gp-outline-primary">
+                        Буцах
+                    </NavLink>
+                    {
+                        check_info
+                        ?
+                            <div className="alert alert-danger position-absolute float-right mr-3" style={{right: "0"}} role="alert">Мэдээлэл байхгүй байна</div>
+                        :
+                            <div></div>
+                    }
+                    <h5 className="mb-3">Лавлах</h5>
+                    <table className="table table-bordered">
+                        <tbody>
+                            <tr>
+                                <td><i className="fa fa-map mr-2" aria-hidden="true"></i>Цэгийн нэр</td>
+                                <td>
+                                    {
+                                        points.map((value, key) => <b key={key}>{'"'+ value.name + '" '}</b>)
+                                    }
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><i className="fa fa-location-arrow mr-2" aria-hidden="true"></i>Планшет</td>
+                                <td><a className="text-info" href={`/payment/download-pdf/${this.state.payment_id}/`}>файл</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><i className="fa fa-location-arrow mr-2" aria-hidden="true"></i>Төлбөр</td>
+                                <td>{points.map((value,key)=><b key={key}>{'"' + value.amount + '" '}</b>)}₮</td>
+                            </tr>
+                            <tr>
+                                <td><i className="fa fa-location-arrow mr-2" aria-hidden="true"></i>Төлбөр</td>
+                                <td>{items.created_at}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div className="row py-3">
+                        <div className="col-md-6 py-0 my-3">
+                        <h5 className="mb-3">Гүйлгээний төлөв</h5>
+                            <ul className="list-unstyled">
+                                <li className="f-nav-item mb-2">
+                                    Гүйлгээний дугаар | {items.geo_unique_number}
+                                </li>
+                                <li className="f-nav-item mb-2">
+                                    НИЙТ МӨНГӨН ДҮН | {items.total}₮
+                                </li>
+                                <li className="f-nav-item mb-2">
+                                    Үр дүн | {items.is_success ? <span className="text-success">Амжилттай</span> : <span className="text-danger">Амжилтгүй</span>}
+                                </li>
+                            </ul>
+                            <button className="btn btn-primary">Хэвлэх</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
--- frontend/profile/src/components/history/historyTable.jsx
+++ frontend/profile/src/components/history/historyTable.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react"
+import {Details} from './details'
+import {Link} from "react-router-dom"
 
 export class HistoryTable extends Component {
     render() {
@@ -6,12 +8,36 @@ export class HistoryTable extends Component {
         const {id, geo_unique_number, total_amount, description, created_at, is_success, success_at, bank_unique_number}=this.props.values
         return (
             <div className="col-4 my-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class={ is_success ? "card-title text-success" : "card-title text-danger"}>{geo_unique_number}</h5>
-                        <p class="card-text">{description}</p>
-                        <p class="card-text">Нийт үнэ {total_amount}</p>
-                        <a className="btn gp-btn-primary" href='#' onClick={this.props.handleDownload}>Татах</a>
+                <div className="card">
+                    <div className="card-body">
+                        <Link to={`/profile/all/api/details/${id}/`}>
+                            <h5>
+                                { is_success
+                                    ?
+                                        <i className="fa fa-check-circle text-success"></i>
+                                    :
+                                        <i className="fa fa-exclamation-triangle text-warning"></i>
+                                }
+                                &nbsp; {description}
+                            </h5>
+                        </Link>
+                        <p className="card-text">{geo_unique_number}</p>
+                        <p className="card-text">Нийт үнэ: {total_amount}₮</p>
+                        <p className="card-text text-muted">{created_at}</p>
+                        {
+                            is_success
+                            ?
+                            <a className="btn gp-outline-primary" href='#' onClick={this.props.handleDownload}>Татах</a>
+                            :
+                            null
+                        }
+                        {
+                            is_success
+                            ?
+                            <a className="btn gp-outline-primary float-right" href={`/payment/download-pdf/${id}/`}>Файл</a>
+                            :
+                            null
+                        }
                     </div>
                 </div>
             </div>
