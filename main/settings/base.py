aa7afe4bc7739ea9ce8503e253fd63ca11eb6569
Baigalmaa Zorigt <49391474+baigalaaZorigt@users.noreply.github.com>
2020-09-28 15:45:31 +0800

    bug

--- main/settings/base.py
+++ main/settings/base.py
@@ -151,7 +151,7 @@ FILES_ROOT = os.path.join(BASE_DIR, 'geoportal_app', 'files', 'payment')
 
 LOGIN_URL = 'secure:login'
 LOGIN_REDIRECT_URL = 'bundle:all'
-LOGIN_REDIRECT_ORG_URL = 'org:all'
+LOGIN_REDIRECT_ORG_URL = 'org:frontend'
 LOGIN_REDIRECT_URL_MOBILE = 'mobile:all'
 LOGOUT_REDIRECT_URL = 'bundle:all'
 
