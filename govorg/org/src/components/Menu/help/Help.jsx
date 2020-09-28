43d94fe181a189cb1e52cef7dad8f6efc8c7e15f
Delgermaa47 <67996697+Delgermaa47@users.noreply.github.com>
2020-09-28 06:43:23 -0500

    qgis plugin install zaawar

--- govorg/org/src/components/Menu/help/Help.jsx
+++ govorg/org/src/components/Menu/help/Help.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import {Switch, Route, Link, NavLink} from "react-router-dom"
+import './help.css'
 export class Help extends Component {
 
     constructor(props) {
@@ -16,7 +17,30 @@ export class Help extends Component {
                               <h4 className='text-center'>Qgis-д plugin суулгах заавар</h4>
                                 <ol>
                                     <li>
-                                        Qgis дээр 'plugin' болон 'plugin reloader' суулгах
+                                        <strong>Qgis дээр 'plugin' болон 'plugin reloader' суулгах</strong>
+                                        <ul>
+                                            <li>
+                                                <h6>Qgis -> Manage and Install Plugins -> Not installed -> Plugin builder <br/>Qgis -> Manage and Install Plugins -> Not installed -> Plugin reloader</h6>
+                                                <img src="/static/assets/image/plugin/plugin.png" /></li>
+                                            <li className="mt-3">
+                                                <img src="/static/assets/image/plugin/plugin_builder.png" /></li>
+                                            <li className="mt-3">
+                                                <img src="/static/assets/image/plugin/plugin_view.png" /></li>
+                                        </ul>                                        
+                                    </li>
+                                    <li>
+                                        <ul>
+                                            <li>
+                                              <strong>Үүсгэсэн &nbsp; "plugin"-ийг  &nbsp;  Qgis-тай холбох</strong>
+                                                <h6>Qgis ->  Manage and Install Plugins -> Installed -> Үүсгэсэн 'plugin'-ний нэрийг сонгоно.</h6>
+                                                <img src="/static/assets/image/plugin/select_plugin.png" />
+                                            </li>
+                                            <li>
+                                              <strong>Үүсгэсэн &nbsp; "plugin"-ийг  &nbsp;  Plugin Reloader-тай холбох. Ингэснээр "Plugin"-ийг дахин дахин ачааллах боломжтой болно. </strong>
+                                                <h6>Qgis ->  Plugin Reloader-> Configure -> Select plugin you want to reload -> Үүсгэсэн plugin.</h6>
+                                                
+                                            </li>
+                                        </ul>
                                     </li>
                                 </ol>
                         </div>
