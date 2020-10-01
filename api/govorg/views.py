790e473ac7ee7f5403a6cebab918bfc58dcb8042
Delgermaa47 <67996697+Delgermaa47@users.noreply.github.com>
2020-10-01 06:36:00 -0500

    conflict

--- api/govorg/views.py
+++ api/govorg/views.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from geojson import Point, Feature, FeatureCollection, dump,MultiPoint
+
 from django.http import HttpResponse, Http404
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, reverse
@@ -11,7 +11,6 @@ from api.utils import filter_layers, replace_src_url
 from backend.govorg.models import GovOrg
 from backend.wms.models import WMS, WMSLog
 from backend.changeset.models import ChangeSet
-from django.db import connections
 
 
 def _get_service_url(request, token, wms):
@@ -79,37 +78,3 @@ def qgis_submit(request):
 
     except Exception:
         return JsonResponse({'success': False})
-
-
-
-def _get_changeset_display(ob):
-    geom= eval(ob[1])
-    geometry = eval(geom['geom'])
-    coordinates = geometry['coordinates']
-    geom_type = geometry['type']
-    return {
-        'coordinate':coordinates,
-        'geom_type':geom_type,
-        'changeset_id':ob[0],
-        'changeset_attributes':ob[2]
-    }
-
-def _get_feature_coll(ob, changeset_list):
-    
-    point = MultiPoint((changeset_list[ob]['coordinate']))
-    return Feature(type = 'Feature', properties={"changeset_id": str(changeset_list[ob]['changeset_id'])}, geometry=point)
-
-
-
-
-cursor = connections['default'].cursor()
-cursor.execute(''' select * from changeset_changeset''')
-changesets = cursor.fetchall()
-feature = []
-geoJson = []
-changeset_list = [_get_changeset_display(ob) for ob in changesets]
-features = [ _get_feature_coll(ob, changeset_list) for ob in range(len(changeset_list))]
-
-feature_collection = FeatureCollection(features)
-with open('myfile.geojson', 'w') as f:
-   dump(feature_collection, f)
