e1b2758b8e0d1eaf1db554145bfd8e39945b35d6
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2020-10-06 08:30:42 +0800

    Sample table list for gov bundle. ref #711

--- govorg/backend/barilga_suurin_gazar/views.py
+++ govorg/backend/barilga_suurin_gazar/views.py
@@ -1,22 +1,25 @@
 import requests
 import json
 from geojson import Feature, FeatureCollection
+
+from django.db import connections
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_GET, require_POST
 
 from backend.changeset.models import ChangeSet
-from django.db import connections
+from backend.org.models import Org
 from main.decorators import ajax_required
 
 
 
 def _get_changeset_display(ob):
-    
+
     geom= eval(ob.geom)
     geometry = eval(geom['geom'])
     coordinates = geometry['coordinates']
     geom_type = geometry['type']
-    
+
     return {
         'coordinate':coordinates,
         'geom_type':geom_type,
@@ -26,7 +29,7 @@ def _get_changeset_display(ob):
     }
 
 def _get_feature_coll(ob, changeset_list):
-    geom_type = changeset_list[ob]['geom_type'] 
+    geom_type = changeset_list[ob]['geom_type']
     if geom_type == 'Point':
         from geojson import Point
         point = Point((changeset_list[ob]['coordinate']))
@@ -71,4 +74,50 @@ def changeset_all(request):
     rsp = {
         'GeoJson': feature_collection,
     }
+    return JsonResponse(rsp)
+
+
+@require_GET
+@ajax_required
+def table_list(request):
+
+    org = get_object_or_404(Org, employee__user=request.user)
+
+    rsp = {
+        'items': [
+            {
+                'schema': 'public',
+                'name': 'AU_SumUnit',
+            },
+            {
+                'schema': 'public',
+                'name': 'AU_StateUnit',
+            },
+            {
+                'schema': 'public',
+                'name': 'AU_AimagUnit',
+            },
+            {
+                'schema': 'public',
+                'name': 'AddressPoint',
+            },
+            {
+                'schema': 'public',
+                'name': 'AdmUnitSum',
+            },
+            {
+                'schema': 'public',
+                'name': 'AdmUnitUls',
+            },
+            {
+                'schema': 'public',
+                'name': 'Sand',
+            },
+            {
+                'schema': 'public',
+                'name': 'Shuudan_uilchilgeenii_salbaruud',
+            },
+        ]
+    }
+
     return JsonResponse(rsp)
