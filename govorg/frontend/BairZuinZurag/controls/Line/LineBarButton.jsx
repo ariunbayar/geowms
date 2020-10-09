4e823695818c899a4842f7e6d292df0d643aeb05
Delgermaa47 <67996697+Delgermaa47@users.noreply.github.com>
2020-10-09 01:35:58 -0500

    add mr

--- govorg/frontend/BairZuinZurag/controls/Line/LineBarButton.jsx
+++ govorg/frontend/BairZuinZurag/controls/Line/LineBarButton.jsx
@@ -20,7 +20,7 @@ export class LineBarButton extends Control {
 
         const elementi = document.createElement('i')
         elementi.setAttribute('aria-hidden', 'true')
-        elementi.className = 'fa fa-minus gp-text-primary fa-2x'
+        elementi.className = 'fa fa-minus gp-text-primary fa-2x ml-2 mt-1'
         elementa.appendChild(elementi)
 
         element.addEventListener('click', (event) => {
