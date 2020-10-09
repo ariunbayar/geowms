ae7db3155a9576e0b044c9938cd6812ab09ca879
Tuguldur88 <43751857+Tuguldur88@users.noreply.github.com>
2020-10-09 14:23:36 +0800

    baiguullaga bundle geom style zasvar

--- govorg/frontend/TeevriinSuljee/Map.jsx
+++ govorg/frontend/TeevriinSuljee/Map.jsx
@@ -117,30 +117,31 @@ export default class TeevriinSuljee extends Component{
             }),
           }),
           'Point': new Style({
-            stroke: new Stroke({
-              color: 'red',
-              width: 10,
-            }),
-            fill: new Fill({
-              color: 'rgba(255, 255, 0, 0.1)',
+            image: new CircleStyle({
+              radius: 5,
+              fill: new Fill({
+                color: 'blue',
+              }),
             }),
           }),
           'LineString': new Style({
             stroke: new Stroke({
               color: 'green',
-              width: 8,
+              width: 2,
             }),
-            fill: new Fill({
-              color: 'rgba(255, 255, 0, 0.1)',
+          }),
+          'MultiLineString': new Style({
+            stroke: new Stroke({
+              color: 'green',
+              width: 2,
             }),
           }),
           'MultiPoint': new Style({
-            stroke: new Stroke({
-              color: 'green',
-              width: 8,
-            }),
-            fill: new Fill({
-              color: 'rgba(255, 255, 0, 0.1)',
+            image: new CircleStyle({
+              radius: 5,
+              fill: new Fill({
+                color: 'orange',
+              }),
             }),
           }),
         };
