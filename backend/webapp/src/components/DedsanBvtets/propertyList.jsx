4d102bd3a72010d7e899fa995384beb62b19e00d
odko77 <63903818+odko77@users.noreply.github.com>
2020-10-21 16:14:18 +0800

    tur

--- backend/webapp/src/components/DedsanBvtets/propertyList.jsx
+++ backend/webapp/src/components/DedsanBvtets/propertyList.jsx
@@ -47,33 +47,34 @@ export default class Propterties extends Component {
         const {property_name, disabled, toggleButton} = this.state
         const {property_id} = this.props
         return (
-            <div>
+            <div className="row">
                 properties : &nbsp;
-                    <input
-                        value={property_name}
-                        onChange={(e) => this.handleChange(e)}
-                        disabled={disabled ? 'disabled' : ''}
-                    />
-                    &nbsp;
-                    {
-                            toggleButton
-                            ?
-                            <i
-                                className="fa fa-floppy-o text-success"
-                                role="button"
-                                onClick={() => this.saveText(property_id)}
-                                aria-hidden="true"
-                                title="Хадгалах"
-                            ></i>
-                            :
-                            <i
-                                className="fa fa-pencil-square-o text-warning"
-                                role="button"
-                                onClick={() => this.textEdit()}
-                                aria-hidden="true"
-                                title="Засах"
-                            ></i>
-                    }
+                <input
+                    value={property_name}
+                    className={`col-10 form-control ` +(disabled ? `-plaintext` : ``)}
+                    onChange={(e) => this.handleChange(e)}
+                    disabled={disabled ? 'disabled' : ''}
+                />
+                &nbsp;
+                {
+                    toggleButton
+                    ?
+                    <i
+                        className="fa fa-floppy-o text-success col-2"
+                        role="button"
+                        onClick={() => this.saveText(property_id)}
+                        aria-hidden="true"
+                        title="Хадгалах"
+                    ></i>
+                    :
+                    <i
+                        className="fa fa-pencil-square-o text-warning"
+                        role="button"
+                        onClick={() => this.textEdit()}
+                        aria-hidden="true"
+                        title="Засах"
+                    ></i>
+                }
             </div>
         )
     }
