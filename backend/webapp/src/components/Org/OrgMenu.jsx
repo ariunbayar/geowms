7b1b771baefff748c614744465fb7a5a957dd025
Tuguldur88 <43751857+Tuguldur88@users.noreply.github.com>
2020-10-22 19:18:20 +0800

    check

--- backend/webapp/src/components/Org/OrgMenu.jsx
+++ backend/webapp/src/components/Org/OrgMenu.jsx
@@ -52,35 +52,33 @@ export class OrgMenu extends Component {
         const org_level = this.props.match.params.level
         const org_id = this.props.match.params.id
         return (
-            <div className="card">
-              <div className="card-body">
-              <ul className="nav nav-tabs nav-tabs-primary nav-justified">
-                            <li className="nav-item">
-                                    <NavLink to={`/back/байгууллага/түвшин/${org_level}/${org_id}/эрх/`}
-                                    activeClassName="active nav-link"  data-toggle="tab">
-                                        <i className="icon-home"></i> <span className="hidden-xs">Эрх</span>
-                                    </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to={`/back/байгууллага/түвшин/${org_level}/${org_id}/хэрэглэгч/`}
-                                activeClassName="active nav-link"  data-toggle="tab">
-                                    <i className="icon-user"></i> <span className="hidden-xs">Албан хаагчид</span></NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to={`/back/байгууллага/түвшин/${org_level}/${org_id}/систем/`}
-                                activeClassName="active nav-link"  data-toggle="tab"><i className="icon-envelope-open">
-                                    </i> <span className="hidden-xs">Систем</span></NavLink>
-                            </li>
-                        </ul>
-                        <div className="tab-content">
-                        <Switch>
-                            <Route path="/back/байгууллага/түвшин/:level/:id/эрх/" component={OrgRole}/>
-                            <Route path="/back/байгууллага/түвшин/:level/:id/хэрэглэгч/" component={OrgUser}/>
-                            <Route path="/back/байгууллага/түвшин/:level/:id/систем/" component={OrgSystem}/>
-                        </Switch>
-                        </div>
-                    </div>
+            <div>
+                <ul className="nav nav-tabs nav-tabs-dark nav-justified">
+                    <li className="nav-item gp-text-primary">
+                        <NavLink to={`/back/байгууллага/түвшин/${org_level}/${org_id}/эрх/`} className="nav-link"
+                            activeClassName="active"  data-toggle="tab">
+                            <i className="icon-home"></i> <span className="hidden-xs">Эрх</span>
+                        </NavLink>
+                    </li>
+                    <li className="nav-item gp-text-primary">
+                        <NavLink to={`/back/байгууллага/түвшин/${org_level}/${org_id}/хэрэглэгч/`} className="nav-link"
+                            activeClassName="active"  data-toggle="tab">
+                        <i className="icon-user"></i> <span className="hidden-xs">Албан хаагчид</span></NavLink>
+                    </li>
+                    <li className="nav-item gp-text-primary">
+                        <NavLink to={`/back/байгууллага/түвшин/${org_level}/${org_id}/систем/`} className="nav-link"
+                                activeClassName="active"  data-toggle="tab"><i className="icon-envelope-open">
+                        </i> <span className="hidden-xs">Систем</span></NavLink>
+                    </li>
+                </ul>
+                <div className="tab-content">
+                    <Switch>
+                        <Route path="/back/байгууллага/түвшин/:level/:id/эрх/" component={OrgRole}/>
+                        <Route path="/back/байгууллага/түвшин/:level/:id/хэрэглэгч/" component={OrgUser}/>
+                        <Route path="/back/байгууллага/түвшин/:level/:id/систем/" component={OrgSystem}/>
+                    </Switch>
                 </div>
+            </div>
         )
     }
 }
