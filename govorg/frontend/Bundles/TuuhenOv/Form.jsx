e05ec106e9c26c75ca504b9f1c29bf5eddf28f9d
Nandiaa <47514814+Nandiaa@users.noreply.github.com>
2020-11-09 18:38:02 +0800

    861 тестийн алдаа засав

--- govorg/frontend/Bundles/TuuhenOv/Form.jsx
+++ govorg/frontend/Bundles/TuuhenOv/Form.jsx
@@ -158,133 +158,131 @@ export class Forms extends Component {
             const has_error = Object.keys(errors).length > 0
             return (
                 <Form>
-                     <Maps handleXY={this.handleXY} coordinatCheck={false} />
-                        <div className='card'>
-                            <div className='card-body row'>
-                                <div className='col-md-9 my-4'>
-                                    <div className="col-md-12 mb-4 my-4">
-                                        <a href="#" className="btn gp-outline-primary" onClick={this.props.history.goBack}>
-                                            <i className="fa fa-angle-double-left"></i> Буцах
-                                        </a>
-                                    </div>
-                                    <div className="row container  my-4">
-                                        <h4>2015 ОНЫ ТҮҮХ, СОЁЛЫН ҮЛ ХӨДЛӨХ ДУРСГАЛЫН ҮЗЛЭГ, ТООЛЛОГЫН ХЭЭРИЙН БҮРТГЭЛИЙН МАЯГТ №1</h4>
-                                    </div>
-                                    <table className="table table-bordered">
-                                        <tbody>
-                                            <tr>
-                                                <th style={{width: "20%"}} scope="row">Дурсгалт газрын бүртгэлийн дугаар</th>
-                                                <td colSpan="2" scope="rowgroup"  >
-                                                    <Field
-                                                        className={'form-control ' + (errors.dugaar ? 'is-invalid' : '')}
-                                                        name='dugaar'
-                                                        id="id_dugaar"
-                                                        type="text"
-                                                    />
-                                                    <ErrorMessage name="dugaar" component="div" className="invalid-feedback"/>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th style={{width: "20%"}} scope="row">Он,сар,өдөр</th>
-                                                <td>
-                                                    <Field
-                                                        className={'form-control ' + (errors.date ? 'is-invalid' : '')}
-                                                        name='date'
-                                                        id="id_date"
-                                                        type="date"
-                                                    />
-                                                    <ErrorMessage name="date" component="div" className="invalid-feedback"/>
-                                                </td>
-                                                <th style={{width: "20%"}}>Бүртгэл хийсэн он сар, өдрийг бичнэ.</th>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Аймаг, Нийслэл</th>
-                                                <td scope="row">
-                                                    <input
-                                                        className='form-control'
-                                                        name='aimagname'
-                                                        id="id_aimagname"
-                                                        value={this.state.aimagname}
-                                                        disabled={true}
-                                                        type="text"
-                                                    />
-                                                </td>
-                                                <th rowSpan="2" scope="rowgroup">Тухайн дурсгал оршиж буй аймаг,<br></br> сумын нэрийг бичнэ.</th>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Сум, Дүүрэг</th>
-                                                <td>
-                                                    <input
-                                                        className='form-control'
-                                                        name='sumname'
-                                                        value={this.state.sumname}
-                                                        disabled={true}
-                                                        id="id_sumname"
-                                                        type="text"
-                                                    />
-                                                </td>
-                                            </tr>
-
-                                            <tr>
-                                                <th scope="row">Хамрах хүруу тоо ширхэг</th>
-                                                {tuuh_id ?
-                                                <td>
-                                                        <button type="button" className="btn btn-outline-success btn-sm btn-round btn-block waves-effect waves-light m-1"  onClick={this.hureeAdd}>Хамрах хүрээ нэмэх</button>
-                                                        <br></br>
-                                                        <hr className="my-2"></hr>
-                                                        {huree_components}
-                                                </td>:
-                                                <td>
-                                                        <Field
-                                                            className={'form-control ' + (errors.too_shirheg ? 'is-invalid' : '')}
-                                                            name='too_shirheg'
-                                                            id="id_too_shirheg"
-                                                            type="number"
-                                                        />
-                                                        <ErrorMessage name="too_shirheg" component="div" className="invalid-feedback"/>
-                                                </td>
-                                                }
-                                                <th>Тоо ширхэг.</th>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Бүртгэгч</th>
-                                                <td>
-                                                    <Field
-                                                        className={'form-control ' + (errors.burtgegch ? 'is-invalid' : '')}
-                                                        name='burtgegch'
-                                                        id="id_burtgegch"
-                                                        type="text"
-                                                    />
-                                                    <ErrorMessage name="burtgegch" component="div" className="invalid-feedback"/>
-                                                </td>
-                                                <th>Бүргэлийг оруулсан хүн.</th>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                    <div className="span3 my-3">
-                                        {has_error
-                                            ?
-                                                <p></p>
-                                            :
-                                            status == 'saved' && !dirty &&
-                                                <p>Амжилттай нэмэгдлээ</p>
-                                        }
-                                        <div>
-                                            <button type="submit" className="btn gp-btn-primary" disabled={isSubmitting || has_error}>
-                                                {isSubmitting && <i className="fa fa-spinner fa-spin"></i>}
-                                                {isSubmitting && <a className="text-light">Шалгаж байна.</a>}
-                                                {!isSubmitting && 'Нэмэх' }
-                                            </button>
-                                        </div>
-                                        <ModalAlert
-                                            modalAction={() => this.modalClose()}
-                                            status={this.state.modal_alert_status}
-                                            title="Амжилттай нэмлээ"
-                                            model_type_icon = "success"
-                                        />
-                                    </div>
+                    <div className='col-md-9 card'>
+                        <Maps handleXY={this.handleXY} coordinatCheck={false} />
+                            {/* <div className='col-md-9 my-4'> */}
+                                <div className="col-md-12 mb-4 my-4">
+                                    <a href="#" className="btn gp-outline-primary" onClick={this.props.history.goBack}>
+                                        <i className="fa fa-angle-double-left"></i> Буцах
+                                    </a>
                                 </div>
-                            </div>
+                                <div className="row container  my-4">
+                                    <h4>2015 ОНЫ ТҮҮХ, СОЁЛЫН ҮЛ ХӨДЛӨХ ДУРСГАЛЫН ҮЗЛЭГ, ТООЛЛОГЫН ХЭЭРИЙН БҮРТГЭЛИЙН МАЯГТ №1</h4>
+                                </div>
+                                <table className="table table-bordered">
+                                    <tbody>
+                                        <tr>
+                                            <th style={{width: "20%"}} scope="row">Дурсгалт газрын бүртгэлийн дугаар</th>
+                                            <td colSpan="2" scope="rowgroup"  >
+                                                <Field
+                                                    className={'form-control ' + (errors.dugaar ? 'is-invalid' : '')}
+                                                    name='dugaar'
+                                                    id="id_dugaar"
+                                                    type="text"
+                                                />
+                                                <ErrorMessage name="dugaar" component="div" className="invalid-feedback"/>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th style={{width: "20%"}} scope="row">Он,сар,өдөр</th>
+                                            <td>
+                                                <Field
+                                                    className={'form-control ' + (errors.date ? 'is-invalid' : '')}
+                                                    name='date'
+                                                    id="id_date"
+                                                    type="date"
+                                                />
+                                                <ErrorMessage name="date" component="div" className="invalid-feedback"/>
+                                            </td>
+                                            <th style={{width: "20%"}}>Бүртгэл хийсэн он сар, өдрийг бичнэ.</th>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Аймаг, Нийслэл</th>
+                                            <td scope="row">
+                                                <input
+                                                    className='form-control'
+                                                    name='aimagname'
+                                                    id="id_aimagname"
+                                                    value={this.state.aimagname}
+                                                    disabled={true}
+                                                    type="text"
+                                                />
+                                            </td>
+                                            <th rowSpan="2" scope="rowgroup">Тухайн дурсгал оршиж буй аймаг,<br></br> сумын нэрийг бичнэ.</th>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Сум, Дүүрэг</th>
+                                            <td>
+                                                <input
+                                                    className='form-control'
+                                                    name='sumname'
+                                                    value={this.state.sumname}
+                                                    disabled={true}
+                                                    id="id_sumname"
+                                                    type="text"
+                                                />
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <th scope="row">Хамрах хүруу тоо ширхэг</th>
+                                            {tuuh_id ?
+                                            <td>
+                                                    <button type="button" className="btn btn-outline-success btn-sm btn-round btn-block waves-effect waves-light m-1"  onClick={this.hureeAdd}>Хамрах хүрээ нэмэх</button>
+                                                    <br></br>
+                                                    <hr className="my-2"></hr>
+                                                    {huree_components}
+                                            </td>:
+                                            <td>
+                                                    <Field
+                                                        className={'form-control ' + (errors.too_shirheg ? 'is-invalid' : '')}
+                                                        name='too_shirheg'
+                                                        id="id_too_shirheg"
+                                                        type="number"
+                                                    />
+                                                    <ErrorMessage name="too_shirheg" component="div" className="invalid-feedback"/>
+                                            </td>
+                                            }
+                                            <th>Тоо ширхэг.</th>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Бүртгэгч</th>
+                                            <td>
+                                                <Field
+                                                    className={'form-control ' + (errors.burtgegch ? 'is-invalid' : '')}
+                                                    name='burtgegch'
+                                                    id="id_burtgegch"
+                                                    type="text"
+                                                />
+                                                <ErrorMessage name="burtgegch" component="div" className="invalid-feedback"/>
+                                            </td>
+                                            <th>Бүргэлийг оруулсан хүн.</th>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <div className="span3 my-3">
+                                    {has_error
+                                        ?
+                                            <p></p>
+                                        :
+                                        status == 'saved' && !dirty &&
+                                            <p>Амжилттай нэмэгдлээ</p>
+                                    }
+                                    <div>
+                                        <button type="submit" className="btn gp-btn-primary" disabled={isSubmitting || has_error}>
+                                            {isSubmitting && <i className="fa fa-spinner fa-spin"></i>}
+                                            {isSubmitting && <a className="text-light">Шалгаж байна.</a>}
+                                            {!isSubmitting && 'Нэмэх' }
+                                        </button>
+                                    </div>
+                                    <ModalAlert
+                                        modalAction={() => this.modalClose()}
+                                        status={this.state.modal_alert_status}
+                                        title="Амжилттай нэмлээ"
+                                        model_type_icon = "success"
+                                    />
+                                </div>
+                            {/* </div> */}
                         </div>
                     </Form>
                     )
