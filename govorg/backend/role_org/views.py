ccfd94416068a574180034df19c65202372b4915
Baigalmaa Zorigt <49391474+baigalaaZorigt@users.noreply.github.com>
2020-11-21 17:22:13 +0800

    feature response

--- govorg/backend/role_org/views.py
+++ govorg/backend/role_org/views.py
@@ -10,6 +10,9 @@ from backend.inspire.models import (
     GovRoleInspire,
     GovPermInspire,
     LProperties,
+    LFeatures,
+    LFeatureConfigs,
+    LDataTypeConfigs,
 )
 
 
@@ -61,6 +64,32 @@ def _get_property_data_display(property_id, feature_id, gov_perm):
           'roles': roles
         }
 
+def _get_all_child_feature(feature_id):
+
+    data_type_ids = LFeatureConfigs.objects.filter(feature_id=feature_id).exclude(data_type_id__isnull=True).values_list('data_type_id', flat=True )
+    properties = []
+    for data_type_id in data_type_ids:
+        l_data_type_configs = LDataTypeConfigs.objects.filter(data_type_id=data_type_id).values_list('property_id', flat=True)
+
+        for l_data_type_config in l_data_type_configs:
+            properties.append(l_data_type_config)
+
+    return len(properties)
+
+
+def _get_feature_data_display(feature_id, property_of_feature):
+
+    feature = get_object_or_404(LFeatures, feature_id=feature_id)
+    all_child = _get_all_child_feature(feature_id)
+
+    return {
+        'id': feature.feature_id,
+        'name': feature.feature_name,
+        'parent_id': feature.package_id,
+        'perm_child_ids': property_of_feature,
+        'all_child':all_child
+    }
+
 
 @require_GET
 @ajax_required
@@ -72,10 +101,23 @@ def views(request):
     feature_ids = GovPermInspire.objects.filter(gov_perm=gov_perm).distinct('feature_id').exclude(feature_id__isnull=True).values_list('feature_id', flat=True)
 
     properties = []
+    property_of_feature = {}
+
     for feature_id in feature_ids:
         property_ids = GovPermInspire.objects.filter(gov_perm=gov_perm, feature_id=feature_id).distinct('property_id').exclude(property_id__isnull=True).values_list('property_id', flat=True)
-
+        property_of_feature[feature_id] = list(property_ids)
         for property_id in property_ids:
             properties.append(_get_property_data_display(property_id, feature_id, gov_perm))
 
-    return JsonResponse({'success': True, 'property': properties})
+    features = [
+        _get_feature_data_display(feature_id, property_of_feature.get(feature_id))
+        for feature_id in feature_ids
+    ]
+
+    rsp = {
+        'success': True,
+        'property': properties,
+        'features': features,
+    }
+
+    return JsonResponse(rsp)
