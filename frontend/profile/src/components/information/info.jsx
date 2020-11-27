7a793716b528395dfd05f4c8b3e9396e05379b61
Nandiaa <47514814+Nandiaa@users.noreply.github.com>
2020-11-27 10:22:10 +0800

    n

--- frontend/profile/src/components/information/info.jsx
+++ frontend/profile/src/components/information/info.jsx
@@ -1,54 +1,77 @@
 import React, {Component} from "react";
-
+import { Link } from 'react-router-dom'
+import {service} from './service'
+import { useHistory } from "react-router-dom";
 
 export  class Info extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      user_detail: [],
+      roles: [],
+      all_role:[],
+      role_id:'',
+      roleId: null,
+      check:false,
+      is_superuser:false,
+      is_active: false,
+      is_modal_limit_open:false,
+      role_name:'',
+      modal_alert_check: "closed",
+      title_name: ""
+    }
+        this.getRole=this.getRole.bind(this)
+        this.handleSaveSuccess=this.handleSaveSuccess.bind(this)
+    }
+
+    componentDidMount() {
+      this.handleSaveSuccess()
   }
 
+  handleSaveSuccess(){
+      service
+      .detail(this.props.match.params.id)
+      .then(({user_detail,roles,all_role}) => {
+          this.setState({user_detail, roles, all_role,is_active: user_detail.is_active, is_superuser:user_detail.is_superuser})
+          roles.map(role=>this.setState({roleId:role.id, role_name:role.name}))
+      })
+  }
+
+  getRole(){
+      const id =this.props.match.params.id
+      const roleId =this.state.role_id
+      const value={'roleId':roleId, 'id':id}
+      service.roleCreate(value).then(({success, item}) => {
+          if (success) {
+              this.handleSaveSuccess()
+              this.setState({check:false})
+
+          }
+      })
+  }
+
+
   render() {
+    const {id, first_name, email,is_sso, last_name, gender, username, last_login, date_joined} = this.state.user_detail
 
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="form-row">
-              <div className="form-group col-md-8">
-                <label htmlFor="password">Хуучин нууц үг:</label>
-                  <input
-                      type="password"
-                      className="form-control"
-                      id="password"
-                      placeholder="Одоогийн нууц үг оруулах"
-                  />
-              </div>
+        <div className="card-body">
+            <div className="row">
+                <div className="col-md-4 mb-4">
+                    <p><strong>Системийн ID</strong>: {id} </p>
+                    <p><strong>Овог</strong>: {last_name} </p>
+                    <p><strong>Нэр</strong>: {first_name} </p>
+                    <p><strong>Хүйс</strong>: {gender} </p>
+                    <p><strong>Цахим хаяг</strong>: {email} </p>
+                    <p><strong>Хэрэглэгчийн нэр</strong>: {is_sso ? <a>{first_name}</a> : <a> {username}</a>} </p>
+                    <p><strong>Бүртгүүлсэн огноо</strong>: {date_joined} </p>
+                    <p><strong>Сүүлд нэвтэрсэн огноо</strong>: {last_login} </p>
+                    <Link to="/gov/profile"> 
+                    <button className="btn gp-btn-primary">Нууц үг солих</button></Link>
+                </div>
             </div>
-            <div className="form-row">
-              <div className="form-group col-md-8">
-                <label htmlFor="re_password">Шинэ нууц үг:</label>
-                  <input
-                      type="password"
-                      className="form-control"
-                      id="new_password"
-                      placeholder="Шинэ нууц үг оруулах"
-                  />
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="form-group col-md-8">
-                <label htmlFor="re_password">Шинэ нууц үг дахин оруулах:</label>
-                  <input
-                      type="password"
-                      className="form-control"
-                      id="re_new_password"
-                      placeholder="Шинэ нууц үг дахин оруулах"
-                  />
-              </div>
-            </div>
-            <button className="btn gp-btn-primary">Хадгалах</button>
-          </div>
         </div>
-      </div>
-    );
+      )
   }
 }
---
+++ frontend/profile/src/components/information/service.jsx
@@ -0,0 +1,100 @@
+export const service = {
+    getAll,
+    detail,
+    userDetailChange,
+    roleCreate,
+    paginatedList
+}
+
+const prefix = '/profile/api/'
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text)
+        if (!response.ok) {
+            if ([401, 403].indexOf(response.status) !== -1) {
+                // TODO auto logout if 401 Unauthorized or 403 Forbidden response returned from api
+                location.reload(true)
+            }
+            const error = (data && data.message) || response.statusText
+            return Promise.reject(error)
+        }
+
+        return data
+    })
+}
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+function _getGetOptions() {
+    return {
+        method: 'GET',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+        },
+    }
+}
+function _getPostOptions() {
+    return {
+        method: 'POST',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-CSRFToken': getCookie('csrftoken'),
+        },
+    }
+}
+
+function getAll(last, first) {
+    const requestOptions = {
+        ..._getPostOptions(),
+    body: JSON.stringify({last, first}),
+    }
+    return fetch(`${prefix}/all/`, requestOptions).then(handleResponse)
+}
+
+
+function detail(id) {
+    const opts = {
+        ..._getGetOptions(),
+    }
+
+    return fetch(`${prefix}/${id}/дэлгэрэнгүй/`, opts).then(handleResponse)
+}
+
+function userDetailChange(id, is_active){
+    const opts = {
+    ..._getPostOptions(),
+    body: JSON.stringify({id, is_active}),
+    }
+    return fetch(`${prefix}/userDetailChange/`, opts).then(handleResponse)
+    }
+
+function roleCreate(payload){
+    const opts = {
+        ..._getPostOptions(),
+        body: JSON.stringify(payload),
+    }
+    return fetch(`${prefix}/roleCreate/`, opts).then(handleResponse)
+}
+
+function paginatedList(page, per_page, query, sort_name) {
+    const requestOptions = {
+        ..._getPostOptions(),
+        body: JSON.stringify({ page, per_page, query, sort_name }),
+    }
+
+    return fetch(`${prefix}/paginatedList/`, requestOptions).then(handleResponse)
+}
