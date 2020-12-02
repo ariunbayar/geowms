d078746d5c3e790f38b329b7b14bb9a0e97c095a
sukhbat98 <60035861+sukhbat98@users.noreply.github.com>
2020-12-02 12:56:43 +0800

    admin нь өөрийн нууц үгээ солих 2020.12.2

--- backend/webapp/src/components/Admin/PasswordChange/PasswordChange.jsx
+++ backend/webapp/src/components/Admin/PasswordChange/PasswordChange.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import { service } from './service'
-import ModalAlert from "../../ModalAlert";
-import axios from 'axios';
 
 export class PasswordChange extends Component {
 
@@ -9,67 +7,39 @@ export class PasswordChange extends Component {
 
         super(props)
         this.state = {
-            modal_alert_status: "closed",
-            timer: null,
-            model_type_icon: "success",
-            msg: "Нууц үг солиход алдаа гарлаа.",
-            reset: false,
-            password: '',
+            is_save_success: null,
+            msg: '',
+            old_password: '',
             new_password: '',
-            renew_password: ''
+            renew_password: '',
+            save_btn_status: 'disabled',
         }
-        this.handSave = this.handSave.bind(this)
-        this.modalClose = this.modalClose.bind(this)
-        this.modalCloseTime = this.modalCloseTime.bind(this)
-        this.handChange = this.handChange.bind(this)
+        this.handleSave = this.handleSave.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    handChange(name, value) {
-        this.setState({[name]: value})
+    handleChange(name, value) {
+        this.setState({[name]: value, is_save_success: null})
+        if (this.state.old_password !== '' && this.state.new_password !== '' && this.state.renew_password !== '' ){
+            this.setState({save_btn_status: 'active'})
+        }
     }
 
-    handSave() {
-        const password = this.state.password
+    handleSave() {
+        const old_password = this.state.old_password
         const new_password = this.state.new_password
         const renew_password = this.state.renew_password
+        this.setState({save_btn_status: 'disabled'})
         if (new_password === renew_password){
-            const values = {"old_password": password, "new_password": new_password}
+            const values = {"old_password": old_password, "new_password": new_password}
             service.passwordChange(values).then(({success, msg, error}) => {
-                if (success) {
-                    this.setState({modal_alert_status: "open", model_type_icon: 'success', msg, reset: true})
-                    this.modalCloseTime()
-                }
-                else{
-                    this.setState({modal_alert_status: "open", model_type_icon: 'danger', msg: error})
-                    this.modalCloseTime()
-                }
+                success ? null: msg = error
+                this.setState({is_save_success: success, msg})
             })
         }
         else{
-            this.setState({modal_alert_status: "open", model_type_icon: 'danger', msg: "Шинэ нууц үг буруу"})
-            this.modalCloseTime()
+            this.setState({is_save_success: false, msg: "Шинэ нууц үг буруу"})
         }
-    }
-
-    modalClose(){
-        this.setState({modal_alert_status: "closed"})
-        clearTimeout(this.state.timer)
-        if (this.state.reset){
-            return (
-                window.location.reload()
-            )
-        }
-    }
-
-    modalCloseTime(){
-        this.state.timer = setTimeout(() => {
-            this.setState({modal_alert_status: "closed"})
-            if (this.state.reset){
-                return (
-                    window.location.reload()
-                )
-            }
-        }, 2000)
     }
 
     render() {
@@ -87,7 +57,7 @@ export class PasswordChange extends Component {
                                 <div className="form-group">
                                     <div className="position-relative has-icon-right">
                                         <label htmlFor="password">Хуучин нууц үг</label>
-                                        <input type="password" className="form-control form-control-rounded" placeholder="Хуучин нууц үгээ оруулна уу" value={this.state.password} onChange={(e) => this.handChange('password', e.target.value)}></input>
+                                        <input type="password" className="form-control form-control-rounded" disabled={this.state.is_save_success ? true : false} placeholder="Хуучин нууц үгээ оруулна уу" value={this.state.old_password} onChange={(e) => this.handleChange('old_password', e.target.value)}></input>
                                         <div className="form-control-position">
                                             <i className="icon-lock"></i>
                                         </div>
@@ -97,7 +67,7 @@ export class PasswordChange extends Component {
                                 <div className="form-group">
                                     <div className="position-relative has-icon-right">
                                         <label htmlFor="new_password">Шинэ нууц үг</label>
-                                        <input type="password" className="form-control form-control-rounded" placeholder="Шинэ нууц үгээ оруулна уу" value={this.state.new_password} onChange={(e) => this.handChange('new_password', e.target.value)}></input>
+                                        <input type="password" className="form-control form-control-rounded" disabled={this.state.is_save_success ? true : false} placeholder="Шинэ нууц үгээ оруулна уу" value={this.state.new_password} onChange={(e) => this.handleChange('new_password', e.target.value)}></input>
                                         <div className="form-control-position">
                                             <i className="icon-lock"></i>
                                         </div>
@@ -106,23 +76,40 @@ export class PasswordChange extends Component {
                                 <div className="form-group">
                                     <div className="position-relative has-icon-right">
                                         <label htmlFor="renew_password">Шинэ нууц үг</label>
-                                        <input type="password" className="form-control form-control-rounded" placeholder="Шинэ нууц үгээ дахин оруулна уу" value={this.state.renew_password} onChange={(e) => this.handChange('renew_password', e.target.value)}></input>
+                                        <input type="password" className="form-control form-control-rounded" disabled={this.state.is_save_success ? true : false} placeholder="Шинэ нууц үгээ дахин оруулна уу" value={this.state.renew_password} onChange={(e) => this.handleChange('renew_password', e.target.value)}></input>
                                         <div className="form-control-position">
                                             <i className="icon-lock"></i>
                                         </div>
                                     </div>
                                 </div>
-                                <button type="button" className="btn btn-primary shadow-primary btn-round btn-block waves-effect waves-light" onClick={() => this.handSave()}>Хадгалах</button>
+                                {this.state.is_save_success !== null ?
+                                    this.state.is_save_success ?
+                                        <div className="alert alert-outline-success alert-dismissible alert-round" role="alert">
+                                            <div className="alert-icon">
+                                                <i className="icon-check ml-3"></i>
+                                            </div>
+                                            <div className="alert-message w-100 text-dark text-center">
+                                            <span>{this.state.msg}</span><br></br>
+                                            <a href="/login/">Та дахин нэвтрэнэ үү.</a>
+                                            </div>
+                                        </div>
+                                        :
+                                        <div className="alert alert-outline-danger alert-dismissible alert-round" role="alert">
+                                            <div className="alert-icon">
+                                                <i className="icon-close ml-3"></i>
+                                            </div>
+                                            <div className="alert-message w-100 text-center">
+                                            <span>{this.state.msg}</span>
+                                            </div>
+                                        </div>
+                                    :
+                                    null
+                                }
+                                <button type="button" className={`btn btn-primary shadow-primary btn-round btn-block waves-effect waves-light ${this.state.save_btn_status === 'active' ? 'active' : 'disabled'}`} onClick={() => this.handleSave()}>Хадгалах</button>
                             </form>
                         </div>
                     </div>
                 </div>
-                <ModalAlert
-                    modalAction={() => this.modalClose()}
-                    status={this.state.modal_alert_status}
-                    title = {this.state.msg}
-                    model_type_icon = {this.state.model_type_icon}
-                />
             </div>
         )
     }
