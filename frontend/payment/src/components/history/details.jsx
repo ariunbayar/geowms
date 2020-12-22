08f30409413b580e527616fdc157cd485801cfab
odko77 <63903818+odko77@users.noreply.github.com>
2020-12-22 17:59:16 +0800

    loading хэсгийг polygon оор авахад бас нэмсэн

--- frontend/payment/src/components/history/details.jsx
+++ frontend/payment/src/components/history/details.jsx
@@ -199,50 +199,66 @@ export class Details extends Component {
                             {
                                 items !== {} && polygon && polygon.length > 0 ?
                                     <table className="table table-bordered">
-                                        <tbody>
-                                            <tr>
-                                                <th className="text-center" colSpan="2" style={{fontSize: '20px'}}>
-                                                    <i className="fa fa-location-arrow mr-2" aria-hidden="true">
-                                                    </i>Давхаргын мэдээлэл
-                                                </th>
-                                            </tr>
-                                            <tr>
-                                                <th>
-                                                    <i className="fa fa-map mr-2" aria-hidden="true"></i>Давхаргын нэр
-                                                </th>
-                                                <th>
-                                                    <i className="fa fa-money mr-2" aria-hidden="true"></i>Давхаргын үнэ
-                                                </th>
-                                            </tr>
+                                        {
+                                            is_loading
+                                            ?
+                                            <tbody>
+                                                <tr>
+                                                    <th>
+                                                        <div className="modal-body height-30">
+                                                            <div className="d-flex justify-content-center">
+                                                                <div className="spinner-border gp-text-primary" role="status"></div>
+                                                            </div>
+                                                        </div>
+                                                    </th>
+                                                </tr>
+                                            </tbody>
+                                            :
+                                            <tbody>
+                                                <tr>
+                                                    <th className="text-center" colSpan="2" style={{fontSize: '20px'}}>
+                                                        <i className="fa fa-location-arrow mr-2" aria-hidden="true">
+                                                        </i>Давхаргын мэдээлэл
+                                                    </th>
+                                                </tr>
+                                                <tr>
+                                                    <th>
+                                                        <i className="fa fa-map mr-2" aria-hidden="true"></i>Давхаргын нэр
+                                                    </th>
+                                                    <th>
+                                                        <i className="fa fa-money mr-2" aria-hidden="true"></i>Давхаргын үнэ
+                                                    </th>
+                                                </tr>
+                                                    {
+                                                        layers.map((value, key) =>
+                                                            <tr key={key}>
+                                                                <td>
+                                                                    <label>{value.name}</label>
+                                                                </td>
+                                                                <td>
+                                                                    <label>{value.amount}₮</label>
+                                                                </td>
+                                                            </tr>
+                                                        )
+                                                    }
+                                                <tr>
+                                                    <td><i className="fa fa-calendar-o mr-2" aria-hidden="true"></i>Огноо:</td>
+                                                    <td>{items.created_at}</td>
+                                                </tr>
                                                 {
-                                                    layers.map((value, key) =>
-                                                        <tr key={key}>
+                                                    items.is_success && items.export_file !== null ?
+                                                        <tr>
+                                                            <td><i className="fa fa-download mr-2" aria-hidden="true"></i>Татах:</td>
                                                             <td>
-                                                                <label>{value.name}</label>
-                                                            </td>
-                                                            <td>
-                                                                <label>{value.amount}₮</label>
+                                                                <a className="text-info" href={`/payment/download-zip/${payment_id}/`}>
+                                                                    Энд дарж татаж авна уу!
+                                                                </a>
                                                             </td>
                                                         </tr>
-                                                    )
+                                                    : null
                                                 }
-                                            <tr>
-                                                <td><i className="fa fa-calendar-o mr-2" aria-hidden="true"></i>Огноо:</td>
-                                                <td>{items.created_at}</td>
-                                            </tr>
-                                            {
-                                                items.is_success && items.export_file !== null ?
-                                                    <tr>
-                                                        <td><i className="fa fa-download mr-2" aria-hidden="true"></i>Татах:</td>
-                                                        <td>
-                                                            <a className="text-info" href={`/payment/download-zip/${payment_id}/`}>
-                                                                Энд дарж татаж авна уу!
-                                                            </a>
-                                                        </td>
-                                                    </tr>
-                                                : null
-                                            }
-                                        </tbody>
+                                            </tbody>
+                                        }
                                     </table>
                                 : null
                             }
