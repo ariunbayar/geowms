b99118bfb488cd1756f81fcdc3937047868a9b78
Ariunbayar B <1480970+ariunbayar@users.noreply.github.com>
2021-01-21 11:51:36 +0800

    Fixes empty geo_json, form_json for migration. ref #1293

--- govorg/backend/org_request/migrations/0013_g_changereq_update_geom_and_form.py
+++ govorg/backend/org_request/migrations/0013_g_changereq_update_geom_and_form.py
@@ -1,8 +1,6 @@
-
-from django.db import migrations, models
-from django.core import serializers
-from django.db import connections
 import json
+
+from django.db import migrations
 
 
 def _convert_text_json(data):
@@ -17,17 +15,19 @@ def update_change_req_forms_and_geom_json_dumps(apps, schema_editor):
 
     ChangeRequest = apps.get_model('org_request', 'ChangeRequest')
     for change_request in ChangeRequest.objects.all():
-        geo_json_jsonloads = _convert_text_json(change_request.geo_json)
-        froms_json_jsonloads = _convert_text_json(change_request.form_json)
-        change_request.geo_json = json.dumps(geo_json_jsonloads, ensure_ascii=False)
-        change_request.form_json = json.dumps(froms_json_jsonloads, ensure_ascii=False)
+        if change_request.geo_json:
+            geo_json_jsonloads = _convert_text_json(change_request.geo_json)
+            change_request.geo_json = json.dumps(geo_json_jsonloads, ensure_ascii=False)
+        if change_request.form_json:
+            froms_json_jsonloads = _convert_text_json(change_request.form_json)
+            change_request.form_json = json.dumps(froms_json_jsonloads, ensure_ascii=False)
         change_request.save()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('org_request', '0012_auto_20201128_0917'),
+        ('org_request', '0013_fix_kind_update_and_kind_delete'),
     ]
 
     operations = [
