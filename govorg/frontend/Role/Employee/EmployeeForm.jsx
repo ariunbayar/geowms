9be57f75a1ff0856b38ce4057f6077d50b404223
sukhbat98 <60035861+sukhbat98@users.noreply.github.com>
2021-01-22 19:38:40 +0800

    employee хүснэгтийг цэвэрлэв

--- govorg/frontend/Role/Employee/EmployeeForm.jsx
+++ govorg/frontend/Role/Employee/EmployeeForm.jsx
@@ -3,7 +3,6 @@ import React, { Component } from "react"
 import {EmployeeTable} from './EmployeeTable'
 import { service } from "./service"
 import { NavLink } from "react-router-dom"
-import { Notif } from "@utils/Notification"
 
 
 export class EmployeeForm extends Component {
@@ -11,15 +10,11 @@ export class EmployeeForm extends Component {
     constructor(props) {
         super(props)
 
-        this.too = 0;
         this.state={
             employees:[],
             prefix: '/gov/perm/employee',
         }
         this.getList = this.getList.bind(this)
-        this.handleRemove = this.handleRemove.bind(this)
-        this.addNotif = this.addNotif.bind(this)
-        this.handleTokenRefresh = this.handleTokenRefresh.bind(this)
     }
 
     getList() {
@@ -34,41 +29,6 @@ export class EmployeeForm extends Component {
 
     componentDidMount() {
         this.getList()
-    }
-
-    handleRemove(id) {
-        service
-            .deleteEmployee(id)
-            .then(({ success }) => {
-                if(success) {
-                    this.addNotif('success', 'Амжилттай устгалаа', 'check')
-                    this.getList()
-                }
-            })
-    }
-
-    handleTokenRefresh(id) {
-        service
-            .empTokenRefresh(id)
-            .then(({ success, info }) => {
-                if(success) {
-                    this.addNotif('success', info, 'check')
-                    this.getList()
-                }
-                else {
-                    this.addNotif('danger', info, 'times')
-                }
-            })
-    }
-
-    addNotif(style, msg, icon){
-        this.too ++
-        this.setState({ show: true, style, msg, icon })
-        const time = setInterval(() => {
-            this.too --
-            this.setState({ show: true })
-            clearInterval(time)
-        }, 2000);
     }
 
     render() {
@@ -91,11 +51,8 @@ export class EmployeeForm extends Component {
                                             <th scope="col">Овог нэр</th>
                                             <th scope="col">Имэйл</th >
                                             <th scope="col">Албан тушаал</th >
-					    <th scope="col">Role</th>
+					                        <th scope="col">Role</th>
                                             <th className="text-center" scope="col">Админ эсэх</th >
-                                            <th className="text-center" scope="col">Токен шинэчлэх</th>
-                                            <th className="text-center" scope="col">Засах</th>
-                                            <th className="text-center" scope="col">Устгах</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -104,8 +61,6 @@ export class EmployeeForm extends Component {
                                                 idx={idx + 1}
                                                 values={item}
                                                 prefix={prefix}
-                                                handleRemove={() => this.handleRemove(item.id)}
-                                                handleTokenRefresh={() => this.handleTokenRefresh(item.id)}
                                             />
                                         )}
                                     </tbody>
@@ -114,7 +69,6 @@ export class EmployeeForm extends Component {
                         </div>
                     </div>
                 </div>
-                <Notif show={this.state.show} too={this.too} style={this.state.style} msg={this.state.msg} icon={this.state.icon}/>
             </div>
         )
     }
