09458ab7331d345dc616b57df6e1d8b803857cab
sukhbat98 <60035861+sukhbat98@users.noreply.github.com>
2021-01-26 19:13:25 +0800

    ModalAlert

--- frontend/bundle/src/components/DetailPage/BundleMap.jsx
+++ frontend/bundle/src/components/DetailPage/BundleMap.jsx
@@ -474,7 +474,6 @@ export default class BundleMap extends Component {
                         })
                 } else {
                     /* TODO */
-                    console.log('no feature url', wms_source);
                 }
             })
         })
@@ -637,7 +636,6 @@ export default class BundleMap extends Component {
     };
 
     calcPrice(feature_geometry, layer_info, coodrinatLeftTop_map_coord, coodrinatRightBottom_map_coord, feature_info_list) {
-        alert("hahaha")
         const area = this.formatArea(feature_geometry)
         var layer_list = []
         layer_info.wms_list.map((w, idx) => {
