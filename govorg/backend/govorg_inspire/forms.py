40b7b2faef998c46d2eba4d2c180f61e75331840
Nandiaa <47514814+Nandiaa@users.noreply.github.com>
2021-01-28 08:43:45 +0800

    хадгалав

---
+++ govorg/backend/govorg_inspire/forms.py
@@ -0,0 +1,15 @@
+# from django import forms
+
+# from .models import GovOrg as Order
+
+
+# class OrderForm(forms.ModelForm):
+
+#     class Meta:
+
+#         model = Order
+
+#         fields = [
+#             'name',
+#             'org',
+#         ]
