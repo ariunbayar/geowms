26aaf8427bc50ae8c614d74d0dde8389f3d97afd
odko77 <63903818+odko77@users.noreply.github.com>
2021-02-16 17:32:31 +0800

    шинэ таб нэмээд түүнийгээ шинэ үүсгэсэн файлтайгаа холбосон

---
+++ backend/webapp/src/components/Org/OrgUsersAddress/UsersAddress.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+export default class UsersAddress extends Component {
+
+    render() {
+        console.log("lol");
+        return (
+            <div className="my-4">
+                <h1>LOL</h1>
+            </div>
+        )
+    }
+}
