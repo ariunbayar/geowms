1c9c78ac8fddfa60c844202257a089dcdb0e19d2
odko77 <63903818+odko77@users.noreply.github.com>
2021-03-04 15:31:02 +0800

    gov хамрах хүрээ зассан

--- govorg/frontend/Role/Region/index.jsx
+++ govorg/frontend/Role/Region/index.jsx
@@ -1,22 +1,97 @@
 import React, { Component } from "react"
-import { Switch, Route } from "react-router-dom"
+import { Map, View } from 'ol'
 
-import MapRegion from './MapRegion'
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style'
+import GeoJSON from 'ol/format/GeoJSON';
+import {Vector as VectorSource} from 'ol/source';
+import {Vector as VectorLayer} from 'ol/layer';
+import 'ol/ol.css'
+import { service } from './service'
 
-export default class Region extends Component {
+export default class MapRegion extends Component {
 
     constructor(props) {
         super(props)
+
+        this.state = {
+            dataProjection: 'EPSG:4326',
+            featureProjection: 'EPSG:3857',
+        }
+
+        this.loadMap = this.loadMap.bind(this)
+        this.loadMapData = this.loadMapData.bind(this)
+        this.getRegion = this.getRegion.bind(this)
+
+    }
+
+    componentDidMount() {
+        this.loadMap()
+    }
+
+    getRegion() {
+        service
+            .getRegion()
+            .then(({ allowed_geom }) => {
+                this.loadMapData(allowed_geom)
+            })
+    }
+
+    loadMap() {
+        const map = new Map({
+          layers: [
+              new TileLayer({
+                source: new OSM(),
+              })
+          ],
+          target: 'map',
+          view: new View({
+              center: [11461613.630815497, 5878656.0228370065],
+              zoom: 5.041301562246971,
+          }),
+        });
+        this.map = map
+        this.getRegion()
+    }
+
+    loadMapData(GeoJson){
+
+        const styles_new = {
+          'MultiPolygon': new Style({
+            stroke: new Stroke({
+              color: 'green',
+              width: 2,
+            }),
+            fill: new Fill({
+              color: 'rgba(0,255,0,0.3)',
+            }),
+          }),
+        };
+        const geojsonObject =  GeoJson
+        var features_new =  new GeoJSON().readFeatures(geojsonObject, {
+            dataProjection: this.state.dataProjection,
+            featureProjection: this.state.featureProjection,
+        });
+        const vectorSourceNew = new VectorSource({
+            features: features_new
+        });
+
+        const vectorLayerNew = new VectorLayer({
+            source: vectorSourceNew,
+            style: function (feature) {
+            return styles_new[feature.getGeometry().getType()];
+            }
+        });
+        this.map.addLayer(vectorLayerNew)
+        this.map.getView().fit(features_new[0].getGeometry(),{ padding: [300, 300, 300, 300] })
     }
 
     render() {
-        const { allowed_geom } = this.props
         return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-12">
-                        <MapRegion geom={ allowed_geom }/>
-                    </div>
+            <div className="card">
+                <div className="card-body">
+                    <div id="map" style={{height: 'calc( 98vh - 85px - 15px)'}}></div>
                 </div>
             </div>
         )
