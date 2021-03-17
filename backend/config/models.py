0b9a03a636021df798c2a610ad57d84a4cfa7b27
odko77 <63903818+odko77@users.noreply.github.com>
2021-03-17 10:42:05 +0800

    covid config ыг хадгалах тусдаа model үүсгэж түүнийг байгууллагад оноож өгөхөөр тохиргоо хийсэн!

--- backend/config/models.py
+++ backend/config/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from backend.org.models import Org
 
 class Config(models.Model):
 
@@ -10,6 +11,21 @@ class Config(models.Model):
 
     name = models.CharField(max_length=250, db_index=True, verbose_name='Гарчиг')
     value = models.TextField(verbose_name='Товч агуулга')
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CovidConfig(models.Model):
+
+    class Meta:
+        db_table = "config_covid"
+        ordering = ('id',)
+
+    name = models.CharField(max_length=250, db_index=True, verbose_name='Гарчиг')
+    value = models.TextField(verbose_name='Товч агуулга')
+    org = models.ForeignKey(Org, on_delete=models.PROTECT, null=True, verbose_name="Байгууллага")
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
