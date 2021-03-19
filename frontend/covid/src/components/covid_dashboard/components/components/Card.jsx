55b02287fcb869de1ab910e2c2402a38c9e031c0
Batsukh Tuguldur <tooktogimac@Batsukhs-MacBook-Pro.local>
2021-03-19 10:40:41 +0800

    S

--- frontend/covid/src/components/covid_dashboard/components/components/Card.jsx
+++ frontend/covid/src/components/covid_dashboard/components/components/Card.jsx
@@ -8,7 +8,7 @@ class Card extends PureComponent {
             <div className="col-12 col-md-4 col-lg-4 col-xl-3">
               <div className={`card text-center border-bottom-sm border-top-sm border-${color}`}>
                 <div className="card-body">
-                    <h6>{head_text}</h6>
+                    <h6 style={{minHeight: '40px'}}>{head_text}</h6>
                     {/* <h4 className={`text-${color}`}>{body_text}</h4> */}
                     <div className={`delta bg-${color}`}>{footer_text}</div>
                 </div>
