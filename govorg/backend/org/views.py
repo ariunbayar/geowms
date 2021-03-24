18f19145612e489f8d804c5792d3d29327d8cf69
Tuguldur88 <43751857+Tuguldur88@users.noreply.github.com>
2021-03-24 20:59:47 +0800

    def too covid

--- govorg/backend/org/views.py
+++ govorg/backend/org/views.py
@@ -213,11 +213,38 @@ def _get_form_fields(Model, org):
         for f in Model._meta.get_fields():
             if 'too' in f.name:
                 if hasattr(f, 'verbose_name') and hasattr(f, 'max_length'):
-                    send_fields.append({
-                        'origin_name': f.name,
-                        'name': f.verbose_name,
-                        'length': f.max_length,
-                    })
+                    if f.name == 'emiin_sangiin_too':
+                        send_fields.append({
+                            'origin_name': f.name,
+                            'name': f.verbose_name,
+                            'length': f.max_length,
+                            'is_togtmol': True,
+                            'togtmol_too': utils.get_covid_config('emiin_sangiin_too') or 9026
+                        })
+                    elif f.name == 'emnelegiin_too':
+                        send_fields.append({
+                            'origin_name': f.name,
+                            'name': f.verbose_name,
+                            'length': f.max_length,
+                            'is_togtmol': True,
+                            'togtmol_too': utils.get_covid_config('emlegiin_too') or 2026
+                        })
+                    elif f.name == 'niit_eruul_mendiin_baiguullaga_too':
+                        send_fields.append({
+                            'origin_name': f.name,
+                            'name': f.verbose_name,
+                            'length': f.max_length,
+                            'is_togtmol': True,
+                            'togtmol_too': utils.get_covid_config('niit_eruul_mend_baiguullaga_too') or 4026
+                        })
+                    else:
+                        send_fields.append({
+                            'origin_name': f.name,
+                            'name': f.verbose_name,
+                            'length': f.max_length,
+                            'is_togtmol': False,
+                            'togtmol_too': 123
+                        })
     return send_fields
 
 
