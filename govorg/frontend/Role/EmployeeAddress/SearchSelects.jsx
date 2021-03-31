9847b00262b9bc723e128b66dd1dc4a3dbbc74ea
sukhbat98 <60035861+sukhbat98@users.noreply.github.com>
2021-03-31 12:43:28 +0800

    Ажилчидын хаягыг харуулах scroll

--- govorg/frontend/Role/EmployeeAddress/SearchSelects.jsx
+++ govorg/frontend/Role/EmployeeAddress/SearchSelects.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import {service} from '../Employee/service'
+import './style.css'
 
 export default class SearchSelects extends Component {
 
@@ -180,7 +181,7 @@ export default class SearchSelects extends Component {
                         >
                             Байгууллага/Түвшин
                         </button>
-                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                        <div className="dropdown-menu dropdown-menu-right bundle-view-scroll" aria-labelledby="dropdownMenuButton">
                             {
                                 levels.length > 0 &&
                                     <>
