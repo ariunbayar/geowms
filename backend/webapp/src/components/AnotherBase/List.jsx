5f55bd22d5f1438619fb0c4c86e7102c84602cd4
sukhbat98 <60035861+sukhbat98@users.noreply.github.com>
2021-04-13 17:58:22 +0800

    base refresh catch

--- backend/webapp/src/components/AnotherBase/List.jsx
+++ backend/webapp/src/components/AnotherBase/List.jsx
@@ -89,21 +89,6 @@ export default class List extends Component {
                     )
                 }
             })
-            .catch(() => {
-                this.setState({is_loading: false})
-                this.modalChange(
-                    'fa fa-check-circle',
-                    null,
-                    'danger',
-                    'Алдаа гарсан байна',
-                    ``,
-                    false,
-                    '',
-                    '',
-                    null,
-                    null
-                )
-            })
     }
 
     handleMongo(values){
