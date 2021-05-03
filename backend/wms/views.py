1cd8fee5bb36a253ff81c9cfe7fbd714d8c764ae
sukhbat98 <60035861+sukhbat98@users.noreply.github.com>
2021-05-03 16:48:51 +0800

    wms url нь domain нэрээр байхад layer-үүдээ авахгүй байсан алдаа

--- backend/wms/views.py
+++ backend/wms/views.py
@@ -299,7 +299,12 @@ def proxy(request, wms_id):
     wms = get_object_or_404(WMS, pk=wms_id)
     queryargs = request.GET
     headers = {**BASE_HEADERS}
-    rsp = requests.get(wms.url, queryargs, headers=headers)
+    url = wms.url
+    if 'https://geo.nsdi.gov.mn/' in url:
+        splited_url = url.split('https://geo.nsdi.gov.mn/')
+        if 1 < len(splited_url):
+            url = 'http://192.168.10.15:8080/' + splited_url[1]
+    rsp = requests.get(url, queryargs, headers=headers)
     content = rsp.content
 
     if request.GET.get('REQUEST') == 'GetCapabilities':
