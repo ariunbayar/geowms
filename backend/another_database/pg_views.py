848c9919ca07a8ad1f586501e2a5fed730951914
Delgermaa <67996697+Delgermaa47@users.noreply.github.com>
2021-05-05 18:01:20 +0800

    conflict

--- backend/another_database/pg_views.py
+++ backend/another_database/pg_views.py
@@ -495,10 +495,8 @@ def _insert_to_someone_db(table_name, cursor, columns, feature_code, pg_schema):
 
         geo_data = data['geo_data']
         geo_data = _geojson_to_geom(geo_data)
-        # try:
-        geo_data = utils.convert_3d_with_srid(geo_data)
-
-<<<<<<< HEAD
+        try:
+            geo_data = utils.convert_3d_with_srid(geo_data)
             insert_query = '''
                 INSERT INTO {schema}.{table_name}(
                     geo_id, geo_data, feature_id, {columns}
@@ -517,25 +515,6 @@ def _insert_to_someone_db(table_name, cursor, columns, feature_code, pg_schema):
             success_count = success_count + 1
         except Exception:
             pass
-=======
-        insert_query = '''
-            INSERT INTO public.{table_name}(
-                geo_id, geo_data, feature_id, {columns}
-            )
-            VALUES ('{geo_id}', '{geo_data}', {feature_id}, {columns_data});
-            '''.format(
-                table_name=table_name,
-                geo_id=data['geo_id'],
-                geo_data=geo_data,
-                feature_id=feature_id,
-                columns=','.join(fields),
-                columns_data=', '.join(property_data)
-            )
-        cursor.execute(insert_query)
-        success_count = success_count + 1
-        # except Exception:
-        #     pass
->>>>>>> 63e0ca5daab0e38a45b4436e828ec818fa5589ff
     failed_count = total_count - success_count
     return success_count, failed_count, total_count
 
