a77813dcdb0e2c71511da02c1b6a7ce5c6215474
Nandiaa <47514814+Nandiaa@users.noreply.github.com>
2021-05-21 15:47:48 +0800

    Хуудас болгоныг үүсгэв

---
+++ llc/frontend/Request/Detail.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+
+export class Detail extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>aaaaaaaaaaaaaaa</h1>
+            </div>
+        );
+    }
+}
