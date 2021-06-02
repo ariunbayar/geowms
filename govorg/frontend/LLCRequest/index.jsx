2d498a4652b57a80d4969a9aba408e0f9f74108e
Delgermaa <67996697+Delgermaa47@users.noreply.github.com>
2021-06-02 16:54:57 +0800

    detail hassan

--- govorg/frontend/LLCRequest/index.jsx
+++ govorg/frontend/LLCRequest/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import {Switch, Route} from "react-router-dom"
 import {Detail} from './Detail'
-import {RequestAdd} from '../../../llc/frontend/Request/RequestAdd'
 import {LLCSettings} from './sett_comp'
 
 export default class LLCRequest extends Component {
@@ -14,7 +13,6 @@ export default class LLCRequest extends Component {
         return (
             <Switch>
                 <Route exact path="/gov/llc-request/" component={Detail}/>
-                <Route path="/gov/llc-request/:id/detail/" component={RequestAdd}/>
                 <Route path="/gov/llc-request/:id/configure-bundle/" component={LLCSettings}/>
             </Switch>
         )
