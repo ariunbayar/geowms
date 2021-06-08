032d23d313d3df66043e965056caa34211ec1845
Delgermaa <67996697+Delgermaa47@users.noreply.github.com>
2021-06-08 17:06:25 +0800

    model table body zassn

--- govorg/frontend/LLCRequest/detailModal.jsx
+++ govorg/frontend/LLCRequest/detailModal.jsx
@@ -14,16 +14,25 @@ export default class DetailModal extends Component {
                 <div className="row">
                     <div className="col-md-12 overflow-auto text-justify my-2" style={{height:"calc(40vh - 35px - 7px)"}}>
                         <table className="table table_wrapper_table">
+                            <thead>
+                                <tr>
+                                    <th scope="col"> # </th>
+                                    <th scope="col"> Нэр</th>
+                                    <th scope="col"> Утга</th>
+                                </tr>
+                            </thead>
                             <tbody>
                                 {
                                     values
                                     ?
                                         Object.keys(values).map((layer, idx) =>
-                                            <tr className="p-0" style={{fontSize: '16px'}} key={idx}>
-                                            <th className="font-weight-normal">
-                                                {layer}: {values[layer]}
-                                            </th>
-                                        </tr>
+                                            <tr className="col-md-12" style={{fontSize: '12px'}} key={idx}>
+                                                <td><b>{idx+1}</b></td>
+                                                <td><b>{layer}</b></td>
+                                                <td className="font-weight-normal">
+                                                    <b>{values[layer]}</b>
+                                                </td>
+                                            </tr>
                                         )
                                     :
                                         <tr><th>Хоосон байна.</th></tr>
