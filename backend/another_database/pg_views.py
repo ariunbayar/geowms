e8fb6a65a8c6d346e5ef2b50cf44ea02d63beade
Delgermaa <67996697+Delgermaa47@users.noreply.github.com>
2021-06-13 23:03:35 +0800

    geoserver-s shuud duudaj baigaa code-ig zogsooson

--- backend/another_database/pg_views.py
+++ backend/another_database/pg_views.py
@@ -963,12 +963,12 @@ def refresh_view(request, payload):
 
     if table_id :
         ano_db_table_pg = initial_qs.filter(id=table_id).first()
-        _refresh_feature(ano_db_table_pg)
+        # _refresh_feature(ano_db_table_pg)
     else :
         ano_db_table_pg = initial_qs.filter(another_database_id=id)
 
         for item in ano_db_table_pg:
-            _refresh_feature(item)
+            # _refresh_feature(item)
 
     return JsonResponse({
         'success': True
