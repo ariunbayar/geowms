43d602daed957985b55200a6430942cce742d6da
Delgermaa <67996697+Delgermaa47@users.noreply.github.com>
2021-07-06 18:17:44 +0800

    employee-г position байгаа эсэхийг шалгасан

--- govorg/backend/org/views.py
+++ govorg/backend/org/views.py
@@ -210,16 +210,17 @@ def frontend(request):
     has_position = False
     employee = get_object_or_404(Employee, user=request.user)
     org = get_object_or_404(Org, employee=employee)
-    geom = utils.get_geom(org.geo_id, 'MultiPolygon')
     emp_perm = employee.empperm_set.first()
-    if  employee.org.level == 2 and employee.position.name.lower() == 'мэргэжилтэн':
-        has_position = True
+    geom = utils.get_geom(org.geo_id, 'MultiPolygon')
+
+    if  employee.org.level == 2 and employee.position:
+        if employee.position.name.lower() == 'мэргэжилтэн':
+            has_position = True
+
     if emp_perm:
         emp_perm_insp = emp_perm.empperminspire_set
         approve = emp_perm_insp.filter(perm_kind=EmpPermInspire.PERM_APPROVE).first()
         revoke = emp_perm_insp.filter(perm_kind=EmpPermInspire.PERM_REVOKE).first()
-    if org.level == 2 and has_position:
-        has_position = True
 
     context = {
         'org': {
