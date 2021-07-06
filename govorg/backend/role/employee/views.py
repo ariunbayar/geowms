fa722ea34ad1a3f598da291b5a901f6c0e80b711
Delgermaa <67996697+Delgermaa47@users.noreply.github.com>
2021-07-06 17:47:23 +0800

    эрхийн тохиргоо property давхацаж байсныг зассан

--- govorg/backend/role/employee/views.py
+++ govorg/backend/role/employee/views.py
@@ -578,12 +578,14 @@ def _get_emp_perm_display(emp_perm):
         emp_perm_properties = emp_perm_properties.values('property_id', 'perm_kind')
         property_data, perm_list = get_property_data_display(None, feature_id, emp_perm, EmpPermInspire, True)
         properties.append(property_data)
+
         property_perm_count = count_property_of_feature(emp_perm_properties)
         for perm in perm_list:
             kind_name = get_perm_kind_name(perm['kind'])
             property_perm_count[kind_name] = property_perm_count[kind_name] + 1
-        property_of_feature[feature_id] = property_perm_count
 
+        property_of_feature[feature_id] = property_perm_count
+        emp_perm_properties = emp_perm_properties.distinct('property_id')
         for property_id in emp_perm_properties:
             prop = LProperties.objects.get(property_id=property_id['property_id'])
             property_data, perm_list = get_property_data_display(prop, feature_id, emp_perm, EmpPermInspire, False)
