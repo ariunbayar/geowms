3802a0bdce920a910b1e011a41a585ff7e0e22cb
Narangrl <85742185+Narangrl@users.noreply.github.com>
2021-07-06 17:42:47 +0800

    config зассан

--- llc/frontend/Request/LLCList.jsx
+++ llc/frontend/Request/LLCList.jsx
@@ -127,7 +127,6 @@ export class Detail extends Component {
         this.props.history.push(`/llc/llc-request/${values.id}/дэлгэрэнгүй/`)
     }
 
-    
     modalChange(modal_icon, icon_color, title, text, has_button, description) {
         this.setState({
             modal_icon: modal_icon,
@@ -145,7 +144,6 @@ export class Detail extends Component {
             this.setState({ modal_status: 'initial' })
         })
     }
-
 
     refreshData() {
         this.setState({ refresh: !this.state.refresh })
