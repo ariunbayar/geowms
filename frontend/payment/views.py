2ca98bc770d1015048afc633eaf49629434b2a79
odko77 <63903818+odko77@users.noreply.github.com>
2021-07-09 13:01:42 +0800

    cache нэмсэн

--- frontend/payment/views.py
+++ frontend/payment/views.py
@@ -1794,7 +1794,7 @@ def get_geom(request, payload):
     feature = ''
     geo_id = payload.get('geo_id')
 
-    geom = utils.get_geom(geo_id)
+    geom = utils.geo_cache('geom', geo_id, utils.get_geom(geo_id), 1800)
     if geom:
         geo_json = geom.json
         feature = utils.get_feature_from_geojson(geo_json)
