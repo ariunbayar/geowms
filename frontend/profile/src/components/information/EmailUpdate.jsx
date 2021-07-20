a9431a4b6e5062291a6a623e4817c2ddaf7c42df
odko77 <63903818+odko77@users.noreply.github.com>
2021-07-20 11:58:55 +0800

    syntax ийг сайжруулж html бүтэцлэлт зөв болгосон илүү css file ийг арилгасан

--- frontend/profile/src/components/information/EmailUpdate.jsx
+++ frontend/profile/src/components/information/EmailUpdate.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
-import { service } from './service'
 import Modal from "@utils/Modal/Modal"
 
+import { service } from './service'
 
 export default class EmailUpdate extends Component {
     constructor(props){
         super(props)
         this.state = {
             email: '',
-            error: '',
             modal_status: this.props.modal_status || 'closed',
             user_list: [],
         }
-
 
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -20,29 +18,26 @@ export default class EmailUpdate extends Component {
         this.handleModalOpen = this.handleModalOpen.bind(this)
     }
 
-
-    handleChange = (e) => {
-        const {email} = this.state
-        this.setState({email: e.target.value});
-      }
-
+    handleChange(e) {
+        this.setState({ email: e.target.value });
+    }
 
     handleSubmit() {
         const { email } = this.state
         service
             .updateEmail(email)
             .then(({ success, errors }) => {
-                if(success){
+                if(success) {
                     this.modalChange(
                         'fa fa-check-circle',
                         "success",
                         'Email хаягийг амжилттай шинэчиллээ!',
-                         '',
-                         false,
-                         this.handleModalClose
-                         )
+                        '',
+                        false,
+                        this.handleModalClose
+                    )
                 }
-                else if(errors){
+                else if(errors) {
                     this.modalChange(
                         'fa fa-times-circle',
                         "danger",
@@ -50,13 +45,11 @@ export default class EmailUpdate extends Component {
                         '',
                         false,
                         null
-                        )
+                    )
                 }
             })
-
-            this.setState({ email: '' })
-        }
-
+        this.setState({ email: '' })
+    }
 
     modalChange(modal_icon, icon_color, title, text, has_button, modalClose) {
         this.setState({
@@ -68,9 +61,7 @@ export default class EmailUpdate extends Component {
             modalClose: modalClose
         })
         this.handleModalOpen()
-
     }
-
 
     handleModalOpen() {
         this.setState({ modal_status: 'open' }, () => {
@@ -78,11 +69,9 @@ export default class EmailUpdate extends Component {
         })
     }
 
-
     handleModalClose(){
-        window.location.href = "/profile"
+        window.location.href = "/profile"  // TODO react ashiglana
     }
-
 
     componentDidMount() {
         service
@@ -92,26 +81,24 @@ export default class EmailUpdate extends Component {
             })
     }
 
-
     render() {
         const { email } = this.state.user_list
         return (
             <div className="card">
                 <div className="card-body">
                     <div className="card-header-right">
-                        <p className ="font-weight-bold ">ШИНЭ И-МАЙЛ ХАЯГАА ОРУУЛНА УУ.</p>
+                        <p className ="font-weight-bold">ШИНЭ И-МЭЙЛ ХАЯГАА ОРУУЛНА УУ.</p>
                     </div>
                     <div className="form-group mb-3">
                         <input
-                            error={this.state.error}
                             type="email"
-                            name= "email"
+                            name="email"
                             className="form-control"
                             placeholder= {email}
                             onChange={this.handleChange}>
                         </input>
                     </div>
-                    <button type="submit" className="btn btn-primary" onClick={this.handleSubmit} >Хадгалах</button>
+                    <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Хадгалах</button>
                 </div>
                 <Modal
                     modal_status={this.state.modal_status}
--- frontend/profile/src/components/information/info.jsx
+++ frontend/profile/src/components/information/info.jsx
@@ -1,37 +1,33 @@
 import React, { Component } from 'react'
 import { service } from './service'
-import './info.css'
 
 export class Info extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
-    this.state = {
-        user_list: [],
+        this.state = {
+            user_list: [],
         }
     }
 
-
-  componentDidMount()   {
+    componentDidMount() {
         service
             .userInfo()
-            .then(({user_list}) => {
-                this.setState({user_list})
+            .then(({ user_list }) => {
+                this.setState({ user_list })
             })
-  }
+    }
 
-
-  render() {
-    const{last_name, first_name, email,  username } = this.state.user_list
-    return (
-        <div className="card">
-            <div className="col-sm-12">
-                <div className="card-block">
-                    <div className="card_header">
-                        <h5 className="m-b-20 p-b-5 b-b-default f-w-600">Хэрэглэгчийн мэдээлэл</h5>
-                    </div>
+    render() {
+        const { last_name, first_name, email,  username } = this.state.user_list
+        return (
+            <div className="card">
+                <div className="card-header d-flex align-items-center">
+                    <h5>Хэрэглэгчийн мэдээлэл</h5>
+                </div>
+                <div className="card-body">
                     <div className="row" >
-                        <div className="form-group col-sm-6">
+                        <div className="col-sm-6">
                             <label className="col-form-label font-weight-bold">Хэрэглэгчийн нэр: </label>
                             <span className="input-group-text">{username}</span>
                         </div>
@@ -43,11 +39,9 @@ export class Info extends Component {
                             <label className="col-form-label font-weight-bold">Овог: </label>
                             <span className="input-group-text">{last_name}</span>
                         </div>
-                        <div className="col-sm-6">
+                        <div className="col-sm-6 mt-4">
                             <button className="btn btn-primary" onClick={() => this.props.history.push(`/profile/update-mail/`)}>Цахим хаяг шинэчлэх</button>
                         </div>
-                    </div>
-                    <div className="row">
                         <div className="col-sm-6">
                             <label className="col-form-label font-weight-bold">Нэр: </label>
                             <span className="input-group-text">{first_name}</span>
@@ -55,7 +49,6 @@ export class Info extends Component {
                     </div>
                 </div>
             </div>
-        </div>
-    )
-  }
+        )
+    }
 }
